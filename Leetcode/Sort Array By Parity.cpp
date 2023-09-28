3b56432444715848703af6348aa56af17b708f53
Samar Gupta <139665835+samar0710@users.noreply.github.com>
2023-09-29 03:02:27 +0530

    Update Sort Array By Parity.cpp

--- Leetcode/Sort Array By Parity.cpp
+++ Leetcode/Sort Array By Parity.cpp
@@ -24,3 +24,21 @@ public:
         return nums;
     }
 };
+
+
+
+
+// Approach-2 (Writing your own custom comparator for sorting)
+class Solution {
+public:
+    
+    static bool comparator(int a, int b) {
+        return a%2 < b%2; //a%2 will be 0 if it's even and since we do a%2 < b%2, hence a will come first if it's even
+    }
+    
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        sort(begin(nums), end(nums), comparator);
+        
+        return nums;
+    }
+};
