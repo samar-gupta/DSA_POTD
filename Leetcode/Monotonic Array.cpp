1c7573a5103ac7ab525b001d3c46c713ade8a561
Samar Gupta <139665835+samar0710@users.noreply.github.com>
2023-10-01 15:18:48 +0530

    Create Monotonic Array.cpp

---
+++ Leetcode/Monotonic Array.cpp
@@ -0,0 +1,26 @@
+/*
+      Company Tags                : META (3 Times)
+      Leetcode Link               : https://leetcode.com/problems/monotonic-array/
+*/
+
+/*************************** C++ ***************************/
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int n = nums.size();
+        
+        bool increasing = false;
+        bool decreasing = false;
+        
+        for(int i = 0; i < n-1; i++) {
+            if(nums[i] < nums[i+1]) increasing = true;
+            
+            if(nums[i] > nums[i+1]) decreasing = true;
+        }
+        
+        if(increasing == true && decreasing == true)
+                return false;
+        
+        return true;
+    }
+};
