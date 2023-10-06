cd4c5eeb2a9b08e715abac0024186fe20899069a
Samar Gupta <139665835+samar0710@users.noreply.github.com>
2023-10-06 23:09:54 +0530

    Update Integer Break.cpp

--- Leetcode/Integer Break.cpp
+++ Leetcode/Integer Break.cpp
@@ -1,3 +1,13 @@
+/*
+    Company Tags                : AMAZON
+    Leetcode Link               : https://leetcode.com/problems/integer-break/
+*/
+
+/****************************************************** C++ ***********************************************************/
+//Approach-1 (Recursion  + Memo)
+//T.C : O(n^2) - Because we visit at max n states and at each state we run a for loop from 1 to n-1
+//S.C : O(59) ~ O(1)
+
 class Solution {
 public:
     int t[59];
