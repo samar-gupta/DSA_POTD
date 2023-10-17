98b0f8abaa713a2e8e4e0aea528bdf05079c6bd4
Samar Gupta <139665835+samar0710@users.noreply.github.com>
2023-10-17 19:35:26 +0530

    Create Transitive closure of a Graph.cpp

---
+++ GFG/Transitive closure of a Graph.cpp
@@ -0,0 +1,15 @@
+class Solution{
+public:
+    vector<vector<int>> transitiveClosure(int n, vector<vector<int>> grid)
+    {
+        for(int k=0; k<n; k++) {
+            for(int i=0; i<n; i++) {
+                for(int j=0; j<n; j++) {
+                    if(i == j) grid[i][j]=1;
+                    if(grid[i][k]==1 && grid[k][j]==1) grid[i][j]=1;
+                }
+            }
+        }
+        return grid;
+    }
+};
