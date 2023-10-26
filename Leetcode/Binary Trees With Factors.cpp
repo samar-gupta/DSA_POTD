38fc6dbccd3991a72eb2f1aea4622b12e371f070
Samar Gupta <139665835+samar0710@users.noreply.github.com>
2023-10-27 01:02:35 +0530

    Update Binary Trees With Factors.cpp

--- Leetcode/Binary Trees With Factors.cpp
+++ Leetcode/Binary Trees With Factors.cpp
@@ -25,6 +25,53 @@ public:
 
             for(int j = 0; j<i; j++) {
 
+                int LC = arr[j];                                          //LC : Left Child
+                int RC = arr[i]/LC;                                       //RC : Right Child
+
+                if(root%LC == 0 && mp.find(RC) != mp.end()) {           
+                    mp[root] += mp[LC] * mp[RC];
+                }
+
+            }
+
+        }
+
+        long result = 0;                                        //value -> number of ways of binary trees
+        for(auto &it : mp) {                                   //it.first = value
+            result = (result + it.second) % MOD;              //it.second = ways
+        }
+
+        return result;
+    }
+};
+
+
+
+
+
+
+
+//T.C : O(n^2)   , O(n^2) for 2 loops and O(nlogn) for sorting but since O(n^2) is larger. Hence T.C : O(n^2)
+//S.C : O(n)
+class Solution {
+public:
+
+    int MOD = 1e9+7;
+
+    int numFactoredBinaryTrees(vector<int>& arr) {
+        int n = arr.size();
+
+        sort(begin(arr), end(arr));
+
+        unordered_map<int, long long> mp;
+        mp[arr[0]] = 1;
+
+        for(int i = 1; i<n; i++) {
+            int root = arr[i];
+            mp[root] = 1;
+
+            for(int j = 0; j<i; j++) {
+
                 int LC = arr[j];
 
                 if(root%LC == 0 && mp.find(arr[i]/LC) != mp.end()) {           //RC = arr[i]/LC , (rightChild)
