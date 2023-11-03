3e161e6d0b1be4fa285853796fa4a9bddff841e7
Samar Gupta <139665835+samar0710@users.noreply.github.com>
2023-11-04 03:19:36 +0530

    Create Build an Array With Stack Operations.cpp

---
+++ Leetcode/Build an Array With Stack Operations.cpp
@@ -0,0 +1,31 @@
+/*
+    Company Tags                : will soon update
+    Leetcode Link               : https://leetcode.com/problems/build-an-array-with-stack-operations/
+*/
+
+/************************************************** C++ **************************************************/
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        int stream = 1;
+
+        vector<string> result;
+
+        int i = 0;
+
+        while(i < target.size() && stream <= n) {
+            result.push_back("Push");
+
+            if(stream == target[i]) {
+                i++;
+            } else {
+                result.push_back("Pop");
+            }
+            stream++;
+        }
+
+        return result;
+    }
+};
