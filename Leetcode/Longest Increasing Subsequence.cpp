8d3f8970c51befa02cf17a9c2a6d169754ad3859
Samar Gupta <139665835+samar0710@users.noreply.github.com>
2024-01-05 21:43:02 +0530

    Create Longest Increasing Subsequence.cpp

---
+++ Leetcode/Longest Increasing Subsequence.cpp
@@ -0,0 +1,28 @@
+//Company Tags  : Microsoft
+//Approach - (Using concept of Patience Sorting (O(nlogn))
+//T.C : O(nlogn)
+//S.C : O(n)
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> sorted;
+        
+        for(int i = 0; i<n; i++) {
+            /*
+			     Why lower bound ?
+				 We want increasing subsequence and  hence 
+				 we want to eliminate the duplicates as well.
+				 lower_bound returns iterator to "next greater or equal to"
+			*/
+            auto it = lower_bound(begin(sorted), end(sorted), nums[i]);
+            
+            if(it == end(sorted))
+                sorted.push_back(nums[i]); //greatest : so insert it
+            else
+                *it = nums[i]; //replace
+        }
+        
+        return (int)sorted.size();
+    }
+};
