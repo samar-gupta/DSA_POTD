cb11ac662c713be93647159e0543114065969a5f
Samar Gupta <139665835+samar0710@users.noreply.github.com>
2024-01-16 13:34:15 +0530

    Create Insert Delete GetRandom O(1).cpp

---
+++ Leetcode/Insert Delete GetRandom O(1).cpp
@@ -0,0 +1,44 @@
+//Company Tags      : Amazon, Twitter, Facebook, Uber, Google, Bloomberg, Microsoft
+//Leetcode Link     : https://leetcode.com/problems/insert-delete-getrandom-o1/
+
+
+class RandomizedSet {
+public:
+    /** Initialize your data structure here. */
+    vector<int> vec;
+    unordered_map<int, int> mp;
+    RandomizedSet() {
+        
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    bool insert(int val) {
+        if(mp.find(val) != mp.end())
+            return false;
+        
+        vec.push_back(val);
+        mp[val] = vec.size()-1;
+        return true;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    bool remove(int val) {
+        if(mp.find(val) == mp.end())
+            return false;
+        
+        int idx       = mp[val];
+        int temp      = vec.back();
+        vec.back()    = val;
+        vec[idx]      = temp;
+        mp[temp]      = idx;
+        vec.pop_back();
+        mp.erase(val);
+        return true;
+    }
+    
+    /** Get a random element from the set. */
+    int getRandom() {
+        int idx = rand()%vec.size();
+        return vec[idx];
+    }
+};
