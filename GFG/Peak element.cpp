28ef184c3367470549393ce7510a7d847dfb4ecc
Samar Gupta <139665835+samar0710@users.noreply.github.com>
2024-01-31 12:47:15 +0530

    Create Peak element.cpp

---
+++ GFG/Peak element.cpp
@@ -0,0 +1,43 @@
+//Approach-1
+//T.C - O(n) , S.C : O(1)
+class Solution
+{
+    public:
+    int peakElement(int arr[], int n) {
+       
+       for(int i = 0; i < n-1; i++) {
+           if(arr[i] > arr[i+1]) return i;
+       }
+       
+       return n-1;
+    }
+};
+
+
+
+
+
+//Approach-2
+//T.C - O(logn) , S.C : O(1)
+class Solution
+{
+    public:
+    int peakElement(int arr[], int n) {
+       
+       int start = 0, end = n-1;
+       int mid = start + (end - start)/2;
+       
+       while(start < end) {
+           
+           if(arr[mid] < arr[mid+1]) {
+               start = mid+1;
+           } else {
+               end = mid;
+           }
+           
+           mid = start + (end-start)/2;
+       }
+       
+       return start;
+    }
+};
