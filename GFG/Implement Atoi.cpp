fe215787447407aba5dc17de75115305225495bd
Samar Gupta <139665835+samar0710@users.noreply.github.com>
2024-02-02 18:48:17 +0530

    Create Implement Atoi.cpp

---
+++ GFG/Implement Atoi.cpp
@@ -0,0 +1,23 @@
+//T.C : O(n)
+//S.C : O(1)
+class Solution{
+  public:
+    int atoi(string s) {
+        int ans = 0;
+        int sign = 1;
+        int n = s.size();
+        
+        for(int i = 0; i < n; i++) {
+            if(s[i] == '-' && i == 0) {
+                sign = -1;
+            }
+            else if(s[i] >= '0' && s[i] <= '9') {
+                ans = ans * 10 + (s[i] - '0');
+            }
+            else return -1;
+        }
+        
+        ans = ans * sign;
+        return ans;
+    }
+};
