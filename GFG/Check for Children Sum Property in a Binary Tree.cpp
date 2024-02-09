d3e107d0d66a5325fd14fb674131ddbfae140283
Samar Gupta <139665835+samar0710@users.noreply.github.com>
2024-02-09 20:22:15 +0530

    Create Check for Children Sum Property in a Binary Tree.cpp

---
+++ GFG/Check for Children Sum Property in a Binary Tree.cpp
@@ -0,0 +1,20 @@
+class Solution{
+    public:
+    //Function to check whether all nodes of a tree have the value equal to the sum of their child nodes.
+    int isSumProperty(Node *root)
+    {
+        if((root==NULL)||(root->left==NULL && root->right==NULL)) {
+            return 1;
+        }
+        
+        int sum=0;
+        
+        if(root->left!=NULL) 
+            sum +=root->left->data;
+     
+        if(root->right!=NULL) 
+            sum += root->right->data;
+        
+        return (root->data==sum && isSumProperty(root->left) && isSumProperty(root->right));
+    }
+};
