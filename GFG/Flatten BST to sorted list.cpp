d6d2750782b506e572e8a1878fb2cb89382d06d2
Samar Gupta <139665835+samar0710@users.noreply.github.com>
2024-02-17 02:05:08 +0530

    Update Flatten BST to sorted list.cpp

--- GFG/Flatten BST to sorted list.cpp
+++ GFG/Flatten BST to sorted list.cpp
@@ -1,3 +1,37 @@
+//Approach-1
+//T.C : O(n)
+//S.C : AUxiliary Space is O(1) and Stack Space due to recursion is O(n)
+class Solution
+{
+public:
+    Node *flattenBST(Node *root) {
+        if(!root) {
+            return NULL;
+        }
+        
+        Node* head   = flattenBST(root->left);
+        root->left = NULL;
+        root->right  = flattenBST(root->right);
+        
+        if (head) {
+            Node *temp = head;
+            while (temp->right) {
+                temp = temp->right;
+            }
+            temp->right = root;
+        } else {
+            // If head is NULL, root becomes the head
+            head = root;
+        }
+        
+        return head;
+    }
+};
+
+
+
+
+//Approach-2
 class Solution
 {
 public:
