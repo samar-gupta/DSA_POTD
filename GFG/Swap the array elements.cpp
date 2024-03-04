bea47916ab5c4548cbfefb14c610993c32835c97
Samar Gupta <139665835+samar0710@users.noreply.github.com>
2024-03-04 16:53:44 +0530

    Create Swap the array elements.cpp

---
+++ GFG/Swap the array elements.cpp
@@ -0,0 +1,11 @@
+class Solution{
+  public:
+    void swapElements(int arr[], int n){
+        
+        for(int  i=0;i+2<n;i++){
+            swap(arr[i], arr[i+2]);
+        }
+        return;
+    
+    }
+};
