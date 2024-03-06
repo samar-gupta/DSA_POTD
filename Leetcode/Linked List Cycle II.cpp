1ec6b79e99820c2d2c4d1a14fabc49c0524b9b00
Samar Gupta <139665835+samar0710@users.noreply.github.com>
2024-03-06 15:21:55 +0530

    Create Linked List Cycle II.cpp

---
+++ Leetcode/Linked List Cycle II.cpp
@@ -0,0 +1,34 @@
+//Company Tags  : Microsoft, Paytm, VMWare, Accolite, Amazon, OYO Rooms, Samsung
+                  Snapdeal, D-E-Shaw, Hike, MakeMyTrip, Walmart, MAQ Software, Adobe, SAP Labs, 
+                  Qualcomm, Veritas
+//Leetcode Link : https://leetcode.com/problems/linked-list-cycle-ii/
+
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(!head || !(head->next))
+            return NULL;
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while(fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast)
+                break;
+        }
+        
+        if(fast != slow)
+            return NULL;
+        
+        ListNode* entry = head;
+        while(entry != slow) {
+            entry = entry->next;
+            slow  = slow->next;
+        }
+        return entry;
+    }
+}; 
