051d297d44da14e3e149ae1222ed8694587c8b6d
Samar Gupta <139665835+samar0710@users.noreply.github.com>
2024-03-10 09:13:22 +0530

    Create Remove all duplicates from a given string.cpp

---
+++ GFG/Remove all duplicates from a given string.cpp
@@ -0,0 +1,17 @@
+class Solution{
+public:
+	string removeDuplicates(string str) {
+	    vector<int>vis(256,0);
+	    string ans="";
+	    for(int i=0;i<str.length();i++){
+	        if(vis[str[i]]==1){
+	            continue;
+	        }
+	        else{
+	            vis[str[i]]=1;
+	            ans=ans+str[i];
+	        }
+	    }
+	    return ans;
+	}
+};
