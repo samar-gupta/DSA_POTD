9a18411ab95d0ce6cb2f6faba904e0108af56ce8
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-03-18 14:44:00 +0530

    Create Level order traversal.cpp

---
+++ GFG/Level order traversal.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+    public:
+    //Function to return the level order traversal of a tree.
+    vector<int> levelOrder(Node* root)
+    {
+      //Your code here
+      queue<Node*>q;
+      q.push(root);
+      
+      vector<int>ans;
+      while(!q.empty()){
+          Node* node=q.front();
+          q.pop();
+          
+          ans.push_back(node->data);
+          if(node->left){
+              q.push(node->left);
+          }
+          
+          if(node->right){
+              q.push(node->right);
+          }
+          
+      }
+      return ans;
+    }
+};
