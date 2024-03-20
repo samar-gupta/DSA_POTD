0cc71c79945f5909d139232060ee743a178a5c3a
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-03-20 15:04:34 +0530

    Create Sum of nodes on the longest path from root to leaf node.cpp

---
+++ GFG/Sum of nodes on the longest path from root to leaf node.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int ans=0, mh=0;
+    void solve(Node* root, int h, int s){
+        if(!root){
+            return;
+        }
+        if(!root->left && !root->right){
+            s+=root->data;
+            if(h>mh){
+                ans=s;
+                mh=h;
+            }
+            else if(h==mh){
+                ans=max(ans, s);
+            }
+            return ;
+        }
+        
+        s+=root->data;
+        solve(root->left, h+1, s);
+        solve(root->right, h+1, s);
+        
+        return;
+    }
+    
+    int sumOfLongRootToLeafPath(Node *root)
+    {
+        //code here
+        solve(root, 0, 0);
+        return ans;        
+    }
+};
