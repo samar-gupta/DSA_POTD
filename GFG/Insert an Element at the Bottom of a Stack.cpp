415e463f2146c2426db76693801c71aa014c793a
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-03-24 14:18:30 +0530

    Create Insert an Element at the Bottom of a Stack.cpp

---
+++ GFG/Insert an Element at the Bottom of a Stack.cpp
@@ -0,0 +1,18 @@
+class Solution{
+public:
+    stack<int> insertAtBottom(stack<int> st,int x){
+        stack<int>aux;
+        while(!st.empty()){
+            aux.push(st.top());
+            st.pop();
+        }
+        
+        st.push(x);
+        
+        while(!aux.empty()){
+            st.push(aux.top());
+            aux.pop();
+        }
+        return st;
+    }
+};
