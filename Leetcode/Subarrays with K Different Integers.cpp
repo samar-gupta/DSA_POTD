fb7a7bac7216ba3f0bd92f139dbc6480e50d6de0
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-03-30 18:48:27 +0530

    Update Subarrays with K Different Integers.cpp

--- Leetcode/Subarrays with K Different Integers.cpp
+++ Leetcode/Subarrays with K Different Integers.cpp
@@ -1,6 +1,7 @@
 //Company Tags  : Amazon
 //Leetcode Link : https://leetcode.com/problems/subarrays-with-k-different-integers/
 
+//Approach-1 (Standard Sliding Window twice - A very good Pattern of Sliding Window Problems)
 //T.C : O(n)
 //S.C : O(n)
 class Solution {
@@ -38,5 +39,50 @@ public:
     
     int subarraysWithKDistinct(vector<int>& nums, int k) {
         return slidingWindow(nums, k) - slidingWindow(nums, k-1);
+    }
+};
+
+
+
+//Approach-2 (One Pass Flow)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        unordered_map<int, int> mp;
+        
+        int i_chota  = 0;
+        int j        = 0;
+        int i_bada   = 0;
+        
+        int result   = 0;
+  
+        while(j < n) {
+            mp[nums[j]]++;
+            
+            while(mp.size() > k) {
+                mp[nums[i_chota]]--;
+                if(mp[nums[i_chota]] == 0) {
+                    mp.erase(nums[i_chota]);
+                }
+                i_chota++;
+                i_bada = i_chota;
+            }
+            
+            while(mp[nums[i_chota]] > 1) {
+                mp[nums[i_chota]]--;
+                i_chota++;
+            }
+            
+            if(mp.size() == k) {
+                result += i_chota - i_bada + 1;
+            }
+            j++;
+        }
+        
+        return result;
     }
 };
