dee2a79b18721221a19c0518a46dde4ac42ed479
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-04-12 21:31:47 +0530

    Update Trapping Rain Water.cpp

--- Leetcode/Trapping Rain Water.cpp
+++ Leetcode/Trapping Rain Water.cpp
@@ -35,3 +35,34 @@ public:
         return sum;
     }
 };
+
+
+
+//Approach-2 : (2-Pointer)
+//If two walls are there and we want want to fill water within that wall 
+//we just have to fill the water till min of two wall, if it exceeds the value ,it will overflow.
+//So, we can set 2 pointers left_max, right_max and check which side is greater,
+//if one side is greater iterate on other side because at the min side the water will be filled at its limit.
+//and iterate till pointer is lower than the other one.
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int i = 0, left_max = height[0], sum = 0;
+        int j = height.size() - 1, right_max = height[j];
+        
+        while (i < j) {
+            if (left_max <= right_max) {
+                sum += (left_max - height[i]);
+                i++;
+                left_max = max(left_max, height[i]);
+            } else {
+                sum += (right_max - height[j]);
+                j--;
+                right_max = max(right_max, height[j]);
+            }
+        }
+        
+        return sum;
+    }
+};
