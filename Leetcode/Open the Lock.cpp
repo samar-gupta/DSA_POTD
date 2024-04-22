b659679244b5fb0a5c8b298e7f3634d45d339234
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-04-22 18:32:49 +0530

    Create Open the Lock.cpp

---
+++ Leetcode/Open the Lock.cpp
@@ -0,0 +1,57 @@
+//Company Tags   : META
+//Leetcode Link  : https://leetcode.com/problems/open-the-lock/
+
+//Approach - (Using BFS)
+//T.C : We have n = 10 slots in each wheel and we have w = 4 wheels. In worst case we will explore all possible combinations.
+//      Total Combinations = O(n^w)
+//S.C : In worst case we will store all possible combinations in queue O(n^w)
+class Solution {
+public:
+    void fillNeighbors(queue<string>& que, string& curr, unordered_set<string>& dead) {
+        for(int i = 0; i<4; i++) {
+            char ch  = curr[i];
+
+            char dec = ch=='0' ? '9' : ch-1;
+            char inc = ch=='9' ? '0' : ch+1;
+
+            curr[i]  = dec;
+            if(!dead.count(curr)) {
+                dead.insert(curr);
+                que.push(curr);
+            }
+
+            curr[i] = inc;
+            if(!dead.count(curr)) {
+                dead.insert(curr);
+                que.push(curr);
+            }
+            curr[i] = ch;
+        }
+    }
+    int openLock(vector<string>& deadends, string target) {
+        unordered_set<string> dead(begin(deadends), end(deadends));
+        
+        string start = "0000";        
+        if(dead.count(start))
+            return -1;
+        
+        queue<string> que;
+        que.push(start);
+        
+        int level = 0;
+        while(!que.empty()) {
+            int n = que.size();
+            
+            while(n--) {
+                string curr = que.front();
+                que.pop();
+                if(curr == target)
+                    return level;
+                
+                fillNeighbors(que, curr, dead);
+            }
+            level++;
+        }
+        return -1;
+    }
+};
