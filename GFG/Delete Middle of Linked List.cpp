4e2f50390a4eb10ace1c1708a2265fb755caf2b8
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-04-28 16:54:24 +0530

    Update Delete Middle of Linked List.cpp

--- GFG/Delete Middle of Linked List.cpp
+++ GFG/Delete Middle of Linked List.cpp
@@ -3,6 +3,9 @@ class Solution{
     Node* deleteMid(Node* head)
     {
         // Your Code Here
+        if (head == NULL || head.next == NULL)
+            return NULL;
+        
         Node* fast=head, *slow=head, *prev=NULL;
         while(fast && fast->next){
             fast=fast->next->next;
