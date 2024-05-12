7386df96b8e519c7465fe64f92152b6997c746a3
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-05-12 21:14:16 +0530

    Create Minimum steps to destination.cpp

---
+++ GFG/Minimum steps to destination.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int minSteps(int d) {
+        // code here
+        d=abs(d);
+        
+        int sum=0, steps=0;
+        while(sum<d || abs(sum-d)%2!=0){
+            steps++;
+            sum+=steps;
+        }
+        
+        return steps;
+    }
+};
