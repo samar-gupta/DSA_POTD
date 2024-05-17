5fee406a21991ec46a52e1574d131fd14d3c780c
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-05-17 13:23:31 +0530

    Create Delete Leaves With a Given Value.cpp

---
+++ Leetcode/Delete Leaves With a Given Value.cpp
@@ -0,0 +1,23 @@
+//Company Tags   : Dunzo, Sprinkler
+//Leetcode Link  : https://leetcode.com/problems/delete-leaves-with-a-given-value
+
+//Approach : (Simple Recursion)
+//T.C : O(n)
+//S.C : O(height)
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if(!root) {
+            return NULL;
+        }
+
+        root->left  = removeLeafNodes(root->left, target);
+        root->right = removeLeafNodes(root->right, target);
+
+        if(root->left == NULL && root->right == NULL && root->val == target)
+            return NULL;
+
+        return root;
+        
+    }
+};
