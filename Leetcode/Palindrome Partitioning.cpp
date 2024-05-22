b1ff61faa6c5aac33315171b4ab15f526c9a748c
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-05-22 18:49:32 +0530

    Create Palindrome Partitioning.cpp

---
+++ Leetcode/Palindrome Partitioning.cpp
@@ -0,0 +1,58 @@
+//Company Tags   : Google, Amazon, Microsoft, Meta
+//Leetcode Link  : https://leetcode.com/problems/palindrome-partitioning/
+
+//Whenever a question asks for "Generating all possible" something, think about Backtracking once
+//T.C : O(n * 2^n) - For a string of length n, there are 2^(𝑛 − 1) potential ways to partition it (since each position can either be a cut or not). and we also check palindrome O(n)
+//S.C : O(n * 2^n) - Number of partitions * their length
+class Solution {
+public:
+    int n;
+    
+    bool isPalindrome(string &s, int l, int r) {
+        
+        while(l < r) {
+            if(s[l] != s[r])
+                return false;
+            l++;
+            r--;
+        }
+        
+        return true;
+        
+    }
+    
+    void backtrack(string &s, int idx, vector<string> curr, vector<vector<string>> &result) {
+        
+        if(idx == n) {
+            result.push_back(curr);
+            return;
+        }
+        
+        
+        for(int i = idx; i<n; i++) {
+            
+            if(isPalindrome(s, idx, i)) {
+                
+                curr.push_back(s.substr(idx, i-idx+1));
+                
+                backtrack(s, i+1, curr, result);
+                
+                curr.pop_back();
+                
+            }
+            
+        }
+        
+    }
+    
+    vector<vector<string>> partition(string s) {
+        n = s.length();
+        vector<vector<string>> result;
+        vector<string> curr;
+        
+        backtrack(s, 0, curr, result);
+        
+        return result;
+        
+    }
+};
