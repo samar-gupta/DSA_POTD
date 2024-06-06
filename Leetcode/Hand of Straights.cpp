eb5c3d1f6cf53052624bd8bff64b4f4bf8d096c7
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-06-06 17:17:56 +0530

    Create Hand of Straights.cpp

---
+++ Leetcode/Hand of Straights.cpp
@@ -0,0 +1,40 @@
+//Company Tags  : Google
+//Leetcode Link : https://leetcode.com/problems/hand-of-straights
+
+//Approach : (Simply using ordered map) (to get elements in sorted order)
+//T.C  : O(nlogn) + O(n*groupSize)
+//S.C  : O(n)
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int n = hand.size();
+
+        if(n % groupSize) {
+            return false;
+        }
+
+        map<int, int> mp;
+        for(int &handNumber : hand) {
+            mp[handNumber]++; //O(nlogn)
+        }
+
+        while(!mp.empty()) { //O(n*groupSize)
+
+            int curr = mp.begin()->first; //->second : frequency
+
+            for(int i = 0; i < groupSize; i++) {
+                if(mp[curr + i] == 0) {
+                    return false;
+                }
+
+                mp[curr+i]--;
+                if(mp[curr+i] < 1) {
+                    mp.erase(curr+i);
+                }
+            }
+
+        }
+
+        return true;
+    }
+};
