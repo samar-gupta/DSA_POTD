1f3397689f38e9f31becf060c9f561f8462a07d0
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-06-08 18:39:14 +0530

    Create Continuous Subarray Sum.cpp

---
+++ Leetcode/Continuous Subarray Sum.cpp
@@ -0,0 +1,33 @@
+//Company Tags  : Amazon, Facebook, Paytm
+//Leetcode Link : https://leetcode.com/problems/continuous-subarray-sum/
+
+//Approach : (Using Modulo)
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        unordered_map<int, int> mp;
+        
+        mp[0] = -1;
+        
+        int sum = 0;
+        
+        for(int i = 0; i<n; i++) {
+            sum += nums[i];
+            
+            int remainder = sum%k;
+            
+            if(mp.find(remainder) != mp.end()) {
+                
+                if(i - mp[remainder] >= 2)
+                    return true;
+                
+            } else {
+                mp[remainder] = i;
+            }
+        }
+      
+        return false;
+    }
+};
