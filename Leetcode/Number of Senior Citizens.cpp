68e664c0f40b14e19b8edcdb4b4bd474befc972d
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-08-01 14:51:11 +0530

    Create Number of Senior Citizens.cpp

---
+++ Leetcode/Number of Senior Citizens.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0;
+
+        for(string &detail : details) {
+            int secondDigit = detail[11] - '0';
+            int firstDigit  = detail[12] - '0';
+
+            int age    = secondDigit*10 + firstDigit;
+
+            if(age > 60)
+                count++;
+        }
+
+        return count;
+    }
+};
