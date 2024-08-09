fc1a5181ca8de9da2ad8f1e52eb2993ee0fd3261
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-08-09 16:12:00 +0530

    Create Maximize Array Value After Rearrangement.cpp

---
+++ GFG/Maximize Array Value After Rearrangement.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int Maximize(vector<int> &arr) {
+        // Complete the function
+        int Mod=1e9+7;
+        int ans=0;
+        sort(arr.begin(), arr.end());
+        for(int i=0;i<arr.size();i++){
+            ans=(ans+((long)arr[i]*i)%Mod)%Mod;
+        }
+        return ans;
+    }
+};
