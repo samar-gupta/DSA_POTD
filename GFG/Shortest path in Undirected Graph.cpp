7209f2f7c53ca3c42be4c0087097a033482710f6
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-08-21 18:08:28 +0530

    Update Shortest path in Undirected Graph.cpp

--- GFG/Shortest path in Undirected Graph.cpp
+++ GFG/Shortest path in Undirected Graph.cpp
@@ -1,52 +1,36 @@
 class Solution {
   public:
-    vector<int> shortestPath(int n, int m, vector<vector<int>>& edges) {
-        vector<vector<pair<int,int>>> adj(n+1);
-        for(auto it : edges){
-            adj[it[0]].push_back({it[1],it[2]});
-            adj[it[1]].push_back({it[0],it[2]});
-        }
-    
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        int distance[n+1];
-        int parent[n+1];
-        for(int i=0;i<n+1;i++){
-            distance[i]=INT_MAX;
+    vector<int> shortestPath(vector<vector<int>>& edges, int N,int M, int src){
+        // code here
+        vector<vector<int>> adj(N);
+        // Building the adjacency list for an unweighted graph
+        for (auto it: edges) {
+            adj[it[0]].push_back(it[1]); 
+            adj[it[1]].push_back(it[0]); 
         }
         
-        for(int i=1;i<n+1;i++){
-            parent[i]=i;
-        }
-        distance[1]=0;
-        pq.push({0,1});
-        while(!pq.empty()){
-            int dist = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
-            for(auto it : adj[node]){
-                int edgeWeight = it.second;
-                int adjNode = it.first;
-                if(dist+edgeWeight<distance[adjNode]){
-                    distance[adjNode] = dist+edgeWeight;
-                    pq.push({distance[adjNode],adjNode});
-                    parent[adjNode] = node;
+        vector<int> distance(N, INT_MAX);
+        distance[src] = 0;
+        queue<int> q; q.push(src);
+        // BFS for shortest path in an unweighted graph while (!q.empty()) {
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            for(int adjNode: adj[node]) {
+                if (distance[node] + 1 < distance[adjNode]) { 
+                    distance[adjNode] = distance[node] + 1;
+                    q.push(adjNode);
                 }
             }
         }
-        vector<int> path;
-        
-        if(distance[n]==INT_MAX){
-            path.push_back(-1);
-            return path;
+         
+        // Replace INT_MAX with -1 if a node is unreachable
+        for(int i=0; i<N; i++) {
+            if (distance[i] == INT_MAX) {
+                distance[i] = -1;
+            }
         }
-        int i = n;
-        while (i != 1) {
-        path.push_back(i);
-         i = parent[i];
-         }
-        path.push_back(1);
-        reverse(path.begin(), path.end());
-        path.insert(path.begin(), distance[n]);
-        return path;
+    
+        return distance;
     }
 };
