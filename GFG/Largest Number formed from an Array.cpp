47be564a1750b81ebb03463a33c816f15c455737
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-09-18 17:31:53 +0530

    Update Largest Number formed from an Array.cpp

--- GFG/Largest Number formed from an Array.cpp
+++ GFG/Largest Number formed from an Array.cpp
@@ -1,3 +1,4 @@
+//Approach-1 :
 class Solution{
 public:
 	// The main function that returns the arrangement with the largest value as string.
@@ -16,4 +17,30 @@ public:
         }
         return result;
     }
+};
+
+
+//Approach-2 :
+//T.C : O(nlogn)
+//S.C : O(1) (I am ignoring the space taken by result of size n which we have to return
+class Solution{
+public:
+	string printLargest(int n, vector<string> &arr) {
+	    
+	    auto myComparator = [](string &s1, string &s2) {
+	        if(s1+s2 > s2+s1) {
+	            return true;
+	        }
+	        return false;
+	    };
+	    
+	    sort(begin(arr), end(arr), myComparator);
+	    
+	    string result = "";
+	    for(string &s : arr) {
+	        result += s;
+	    }
+	    
+	    return result;
+	}
 };
