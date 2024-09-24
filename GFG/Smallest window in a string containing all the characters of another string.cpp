adb535aff9a519b4d0ffd5951d861779069d4276
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-09-24 17:02:19 +0530

    Update Smallest window in a string containing all the characters of another string.cpp

--- GFG/Smallest window in a string containing all the characters of another string.cpp
+++ GFG/Smallest window in a string containing all the characters of another string.cpp
@@ -1,3 +1,4 @@
+//Approach-1 :
 class Solution
 {
     public:
@@ -41,5 +42,45 @@ class Solution
             return "-1";
             
         return s.substr(start,ans);
+    }
+};
+
+
+//Approach-2 :
+class Solution
+{
+    public:
+    //Function to find the smallest window in the string s consisting
+    //of all the characters of string p.
+    string smallestWindow (string s, string p)
+    {
+        // Your code here
+        int minLen = INT_MAX;
+        int sind = -1;
+        int r = 0 , l = 0;
+        vector<int> mp(256,0);
+        for(int i=0;i<p.size();i++){
+            mp[p[i]]++;
+        }
+        int cnt  =0;
+        while(r<s.length()){
+            if(mp[s[r]] > 0){
+                cnt++;
+            }
+            mp[s[r]]--;
+            while(cnt == p.length()){
+                if(r-l+1 < minLen){
+                    minLen = r - l +1;
+                    sind = l;
+                }
+                mp[s[l]]++;
+                if(mp[s[l]]>0){
+                    cnt--;
+                }
+                l++;
+            }
+            r++;
+        }
+        return sind == -1 ? "-1" : s.substr(sind,minLen);
     }
 };
