6e856e60eccc8135ac5db7d2a08610675351eaa4
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-09-30 17:20:04 +0530

    Update Merge two BST 's.cpp

--- GFG/Merge two BST 's.cpp
+++ GFG/Merge two BST 's.cpp
@@ -1,3 +1,4 @@
+//Approach-1 : 
 class Solution {
   public:
     // Function to return a list of integers denoting the node
@@ -34,5 +35,47 @@ class Solution {
             ans.push_back(arr2[j++]);
         }
         return ans;
+    }
+};
+
+
+
+//Approach-2 : 
+class Solution
+{
+    public:
+    //Function to return a list of integers denoting the node 
+    //values of both the BST in a sorted order.
+    void inorder(vector<int> &v,Node* root){
+        if(!root) return;
+        inorder(v,root->left);
+        v.push_back(root->data);
+        inorder(v,root->right);
+    }
+    vector<int> merge(Node *root1, Node *root2)
+    {
+       vector<int> v1,v2;
+       inorder(v1,root1);
+       inorder(v2,root2);
+       int i = 0;
+       int j =0;
+       int N = v1.size();
+       int M = v2.size();
+       vector<int> answer;
+       
+       while(i<N && j<M) {
+           if(v1[i] < v2[j])
+               answer.push_back(v1[i++]);
+           else if(v1[i] > v2[j])
+               answer.push_back(v2[j++]);
+           else if(v1[i] == v2[j])
+               answer.push_back(v1[i++]);
+
+       }
+       while(i<N)
+           answer.push_back(v1[i++]);
+       while(j<M)
+           answer.push_back(v2[j++]);
+     return answer;
     }
 };
