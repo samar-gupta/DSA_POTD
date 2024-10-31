605ffe5633423c5d309537dc73623aee2b10ae53
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-11-01 01:06:28 +0530

    Update Minimum Total Distance Traveled.cpp

--- Leetcode/Minimum Total Distance Traveled.cpp
+++ Leetcode/Minimum Total Distance Traveled.cpp
@@ -37,3 +37,55 @@ public:
         return mincost(robot,robot.size()-1,factories,factories.size()-1,mem);
     }
 };
+
+
+
+//Approach-2 : 
+class Solution {
+public:
+    typedef long long ll;
+    
+    ll solve(int ri, int fi, vector<int>& robot, vector<int>& positions, vector<vector<ll>>& t) { 
+        if(ri >= robot.size()) {
+            return 0; //no more distance to be covered
+        }
+        
+        if(fi >= positions.size()) {
+            return 1e12;
+        }
+        
+        if(t[ri] [fi] != -1) {
+            return t[ri] [fi];
+        }
+        
+        ll take_current_factory = abs(robot[ri] - positions[fi]) + solve(ri+1, fi+1, robot, positions, t);
+        ll skip = solve(ri, fi+1, robot, positions, t);
+        
+        return t[ri] [fi] = min(take_current_factory, skip);
+    }
+    
+    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
+        //Step-1
+        sort(begin(robot), end(robot));
+        sort(begin(factory), end(factory));
+        
+        int m = robot.size();
+        
+        //Step-2 (Expand factory positions to avoid tracking of limit and easy recursion)
+        vector<int> positions;
+        for(int i = 0; i < factory.size(); i++) {
+            int limit = factory[i][1];
+            int pos = factory[i][0];
+            
+            for(int j = 0; j < limit; j++) {
+                positions.push_back(pos);
+            }
+        }
+        
+        int n = positions.size();
+        vector<vector<ll>> t(m+1, vector<ll>(n+1, -1));
+        
+        //step-3 (solve)
+        return solve(0, 0, robot, positions, t);
+    }
+};
