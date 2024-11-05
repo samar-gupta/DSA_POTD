9a1afb6d3f55b350c7ec455759c96e743bbf5e21
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-11-05 12:23:06 +0530

    Create Rotate by 90 degree.cpp

---
+++ GFG/Rotate by 90 degree.cpp
@@ -0,0 +1,15 @@
+void rotate(vector<vector<int> >& mat) {
+    // Your code goes here
+    int n=mat.size();
+    int m=mat[0].size();
+    
+    for(int i=0;i<n;i++){
+        
+        for(int j=0;j<i;j++){
+            swap(mat[i][j],mat[j][i]);
+        }
+    }
+    for(int i=0;i<n;i++){
+        reverse(mat[i].begin(),mat[i].end());
+    }
+}
