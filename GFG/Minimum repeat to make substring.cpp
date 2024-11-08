57a45c1b800feaa2106ef8a32b7ca8cbbd9de59b
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-11-08 12:38:14 +0530

    Create Minimum repeat to make substring.cpp

---
+++ GFG/Minimum repeat to make substring.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int minRepeats(string& s1, string& s2) {
+        // code here
+        string temp = s1;
+        int count = 1;
+        while(s1.size()<s2.size())
+        {
+            s1+=temp;
+            count++;
+        }
+        if(s1.find(s2)!=-1)
+        {
+            return count;
+        }
+        s1+=temp;
+        count++;
+        if(s1.find(s2)!=-1)
+        {
+            return count;
+        }
+        return -1;
+    }
+};
