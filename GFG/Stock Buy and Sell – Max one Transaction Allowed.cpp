afb63e60adce97a062db3be5b837c411d302a0a1
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-11-22 18:03:32 +0530

    Create Stock Buy and Sell – Max one Transaction Allowed.cpp

---
+++ GFG/Stock Buy and Sell – Max one Transaction Allowed.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int maximumProfit(vector<int> &prices) {
+        // code here
+        int n=prices.size();
+        int ans=0;
+        if(n==1) return ans;
+        int buy prices[0];
+        for(int i=1;i<n;i++){
+            if(prices[i]<buy){
+                buy-prices[i];
+            }else{
+                ans=max(ans, prices[i]-buy);
+            }
+        }
+        return ans;
+    }
+};
