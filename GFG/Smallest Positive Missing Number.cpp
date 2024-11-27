fee6914eec698e267a97b0fab1a20f328177a45f
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-11-27 15:52:06 +0530

    Create Smallest Positive Missing Number.cpp

---
+++ GFG/Smallest Positive Missing Number.cpp
@@ -0,0 +1,36 @@
+//Approach-1 : 
+//T.C : O(nlogn)
+//S.C : O(1)
+class Solution {
+  public:
+    // Function to find the smallest positive number missing from the array.
+    int missingNumber(vector<int> &arr) {
+        // Your code here
+        int n = arr.size();
+        sort(begin(arr), end(arr));
+       int count=1;
+       for (int i=0;i<n;i++){
+           if (arr[i]==count)
+           count++;
+       }
+       return count;
+    }
+};
+
+
+//Approach-2 : 
+class Solution {
+  public:
+    // Function to find the smallest positive number missing from the array.
+    int missingNumber(vector<int> &arr) {
+        // Your code here
+        int n = arr.size();
+        for(int i=0 ; i<n ; i++)
+            while(arr[i]>0 and arr[i]<=n and arr[i]!=arr[arr[i]-1])
+                swap(arr[i],arr[arr[i]-1]) ;
+                
+        for(int i=0 ; i<n ; i++)
+            if(arr[i]!=i+1) return i+1 ;
+        return n+1 ;
+    }
+};
