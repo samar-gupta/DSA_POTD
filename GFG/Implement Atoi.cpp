dee088807f4e6b89123ec403a5bc4407115811cb
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-11-28 12:47:25 +0530

    Update Implement Atoi.cpp

--- GFG/Implement Atoi.cpp
+++ GFG/Implement Atoi.cpp
@@ -1,3 +1,4 @@
+//old-version
 //T.C : O(n)
 //S.C : O(1)
 class Solution{
@@ -19,5 +20,42 @@ class Solution{
         
         ans = ans * sign;
         return ans;
+    }
+};
+
+
+//new-version
+class Solution {
+  public:
+    int myAtoi(char *s) {
+        // Your code here
+        long long int ans = 0;
+        int sign = 1;
+        int i = 0;
+
+        // Ignore leading whitespace
+        while (s[i] == ' ') {
+            i++;
+        }
+
+        // Check for optional '+' or '-'
+        if (s[i] == '-' || s[i] == '+') {
+            sign = (s[i] == '-') ? -1 : 1;
+            i++;
+        }
+
+        // Process the numerical part
+        while (s[i] >= '0' && s[i] <= '9') {
+            int num = s[i] - '0';
+            ans = ans * 10 + num;
+
+            // Handle overflow/underflow
+            if (sign == 1 && ans >= INT_MAX) return INT_MAX;
+            if (sign == -1 && -ans <= INT_MIN) return INT_MIN;
+
+            i++;
+        }
+
+        return sign * ans;
     }
 };
