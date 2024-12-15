9eec1b124962b2a86d08731f050837ba8b9ccff9
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-12-15 13:23:56 +0530

    Update Peak element.cpp

--- GFG/Peak element.cpp
+++ GFG/Peak element.cpp
@@ -1,58 +1,25 @@
-class Solution
-{
-    public:
-    int peakElement(int arr[], int n)
-    {
-       int l=0,h=n-1;
-       while(l<h){
-           int mid=(l+h)/2;
-           if(arr[mid]<arr[mid+1]) l=mid+1;
-           else h=mid;
-       }
-       return l;
-    }
-};
-
-
-
-//Approach-1
-//T.C - O(n) , S.C : O(1)
-class Solution
-{
-    public:
-    int peakElement(int arr[], int n) {
-       
-       for(int i = 0; i < n-1; i++) {
-           if(arr[i] > arr[i+1]) return i;
-       }
-       
-       return n-1;
-    }
-};
-
-
-
-//Approach-2
-//T.C - O(logn) , S.C : O(1)
-class Solution
-{
-    public:
-    int peakElement(int arr[], int n) {
-       
-       int start = 0, end = n-1;
-       int mid = start + (end - start)/2;
-       
-       while(start < end) {
-           
-           if(arr[mid] < arr[mid+1]) {
-               start = mid+1;
-           } else {
-               end = mid;
-           }
-           
-           mid = start + (end-start)/2;
-       }
-       
-       return start;
+class Solution {
+  public:
+    int peakElement(vector<int> &arr) {
+        // Your code here
+        int n=arr.size();
+        if(n==1)return 0;
+        if(arr[0]>arr[1])return 0;
+        if(arr[n-1]>arr[n-2])return n-1;
+        int s=1;
+        int e=n-2;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            else if(arr[mid]>arr[mid-1]){
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        return -1;
     }
 };
