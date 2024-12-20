826a8a5fec374156448f907a14ee4db51b3aeab7
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-12-20 17:34:52 +0530

    Update Spirally traversing a matrix.cpp

--- GFG/Spirally traversing a matrix.cpp
+++ GFG/Spirally traversing a matrix.cpp
@@ -1,3 +1,4 @@
+//Approach-1 : 
 class Solution {
   public:
     vector<int> spirallyTraverse(vector<vector<int> > &matrix) {
@@ -31,5 +32,54 @@ class Solution {
         }
     
         return ans;
+    }
+};
+
+
+//Approach-2 :
+class Solution {
+  public:
+    vector<int> spirallyTraverse(vector<vector<int> > &matrix) {
+        // code here
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int left=0,right=m-1,bottom=n-1,top=0;
+       int direction=1;
+       vector<int> v;
+       while(left<=right && top<=bottom)
+       {
+           if(direction==1)
+           {
+               for(int i=left;i<=right;i++) 
+                   v.push_back(matrix[top][i]);
+               direction=2;
+               top++;
+           }
+           
+           else if(direction==2)
+           {
+               for(int i=top;i<=bottom;i++)
+                   v.push_back(matrix[i][right]);
+               direction=3;
+               right--;
+           }
+           
+           else if(direction==3)
+           {
+               for(int i=right;i>=left;i--)
+                   v.push_back(matrix[bottom][i]);
+               direction=4;
+               bottom--;
+           }
+           
+           else if(direction==4)
+           {
+               for(int i=bottom;i>=top;i--) 
+                   v.push_back(matrix[i][left]);
+               direction=1;
+               left++;
+           }
+       }
+       return v;
     }
 };
