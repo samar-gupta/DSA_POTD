c1eee3657b338e481d577f317147be4c446d84a8
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-12-21 16:44:55 +0530

    Update Rotate by 90 degree.cpp

--- GFG/Rotate by 90 degree.cpp
+++ GFG/Rotate by 90 degree.cpp
@@ -1,3 +1,4 @@
+//Approach-1 : 
 void rotate(vector<vector<int> >& mat) {
     // Your code goes here
     int n=mat.size();
@@ -15,3 +16,25 @@ void rotate(vector<vector<int> >& mat) {
         reverse(mat[i].begin(),mat[i].end());
     }
 }
+
+
+//Approach-2 : 
+class Solution {
+  public:
+    // Function to rotate matrix anticlockwise by 90 degrees.
+    void rotateby90(vector<vector<int>>& matrix) {
+        // code here
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int j=i;j<m;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+    }
+};
