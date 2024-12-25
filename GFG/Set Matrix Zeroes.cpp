953d7defb964f040504c3d557916ca16447710ea
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2024-12-25 18:54:46 +0530

    Create Set Matrix Zeroes.cpp

---
+++ GFG/Set Matrix Zeroes.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    void setMatrixZeroes(vector<vector<int>> &mat) {
+        // code here
+        int n=mat.size();
+        int m=mat[0].size();
+        
+        vector<int> row(n,0);
+        vector<int> col(m,0);
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0) { 
+                    row[i]=1; 
+                    col[j]=1;
+                }
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(row[i]==1 || col[j]==1) mat[i][j]=0;
+            }
+        }
+    }
+};
