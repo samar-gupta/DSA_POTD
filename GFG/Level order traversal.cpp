3f79c7be01b98edf7343cc6f6bdf997967a2f33d
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-02-02 10:41:31 +0530

    Update Level order traversal.cpp

--- GFG/Level order traversal.cpp
+++ GFG/Level order traversal.cpp
@@ -1,3 +1,38 @@
+//Approach : Using Queue (updated)
+class Solution {
+  public:
+    vector<vector<int>> levelOrder(Node *root) {
+        // Your code here
+        vector<vector<int>> ans;
+        if (!root) return ans;
+        
+        queue<Node*> q;
+        q.push(root);
+        
+        while (!q.empty()) {
+            int size = q.size();
+            vector<int> level;
+            
+            while (size-- > 0) {
+                Node* tmp = q.front();
+                q.pop();
+                level.push_back(tmp->data);
+                
+                if (tmp->left) {
+                    q.push(tmp->left);
+                }
+                if (tmp->right) {
+                    q.push(tmp->right);
+                }
+            }
+            ans.push_back(level);
+        }
+        return ans;
+    }
+};
+
+
+//Approach(old version)
 class Solution
 {
     public:
