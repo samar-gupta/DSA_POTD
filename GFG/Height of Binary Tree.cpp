f5b1ad3d638cb8576cfeb7be44791e2929911c05
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-02-03 19:32:46 +0530

    Update Height of Binary Tree.cpp

--- GFG/Height of Binary Tree.cpp
+++ GFG/Height of Binary Tree.cpp
@@ -1,3 +1,17 @@
+//(updated)
+class Solution {
+  public:
+    // Function to find the height of a binary tree.
+    int height(Node* node) {
+        // code here
+        if (node == NULL)
+            return -1;
+        return 1 + max(height(node->left), height(node->right));
+    }
+};
+
+
+//(old version)
 class Solution{
     public:
     //Function to find the height of a binary tree.
