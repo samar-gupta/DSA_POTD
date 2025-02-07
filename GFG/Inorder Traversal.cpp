cbe0189c6dd0c24dab93faf83de478ac7aaf99bf
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-02-07 09:59:30 +0530

    Create Inorder Traversal.cpp

---
+++ GFG/Inorder Traversal.cpp
@@ -0,0 +1,44 @@
+//Approach-1 : (Using Recursion)
+class Solution {
+  public:
+    
+    void solve(Node* root, vector<int>& ans) {
+        if (root == nullptr)
+            return;
+        solve(root->left, ans);
+        ans.push_back(root->data);
+        solve(root->right, ans);
+    }
+   
+    // Function to return a list containing the inorder traversal of the tree.
+    vector<int> inOrder(Node* root) {
+        // Your code here
+        vector<int> ans;
+        solve(root, ans);
+        return ans;
+    }
+};
+
+
+//Approach-2 : (Using Stack)
+class Solution {
+  public:
+    // Function to return a list containing the inorder traversal of the tree.
+    vector<int> inOrder(Node* root) {
+        // Your code here
+        vector<int> res;
+        stack<Node*> s;
+        Node* curr = root;
+        while(curr or !s.empty()){
+            while(curr){
+                s.push(curr);
+                curr = curr->left;
+            }
+            curr = s.top();
+            s.pop();
+            res.push_back(curr->data);
+            curr = curr->right;
+        }
+        return res;
+    }
+};
