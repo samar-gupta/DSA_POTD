4d5ee2053356666867207c586fcb6fde514fae68
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-02-09 08:49:22 +0530

    Create Maximum path sum from any node.cpp

---
+++ GFG/Maximum path sum from any node.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int solve(Node *root,int &ans){
+        if(root==nullptr) return 0;
+        int left = max(0,solve(root->left,ans));
+        int right = max(0,solve(root->right,ans));
+        ans = max(ans,root->data+left+right);
+        return  root->data + max(left,right);
+    }
+    
+    // Function to return maximum path sum from any node in a tree.
+    int findMaxSum(Node *root) {
+        // code here
+        int ans = INT_MIN;
+        solve(root,ans);
+        return ans;
+    }
+};
