5d29d224392b1dec907b8a4e6c13361e8526ca92
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-02-16 10:45:54 +0530

    Create Construct the Lexicographically Largest Valid Sequence.cpp

---
+++ Leetcode/Construct the Lexicographically Largest Valid Sequence.cpp
@@ -0,0 +1,61 @@
+//Leetcode Link : https://leetcode.com/problems/construct-the-lexicographically-largest-valid-sequence
+
+//Approach (Using simple backtracking Khandani Template)
+//T.C : O(n!)
+//S.C : O(n)
+class Solution {
+public:
+
+    bool solve(int i, int n, vector<int>& result, vector<bool>& used) {
+        if(i >= result.size()) {
+            return true; //gauranteed answer - we were filling largest numbers first
+        }
+
+        if(result[i] != -1) {
+            return solve(i+1, n, result, used);
+        }
+
+        for(int num = n; num >= 1; num--) {
+            if(used[num]) {
+                continue;
+            }
+
+            //try
+            used[num] = true;
+            result[i] = num;
+
+            //EXPLORE
+            if(num == 1) {
+                if(solve(i+1, n, result, used) == true) {
+                    return true;
+                }
+            } else {
+                int j = result[i] + i;
+
+                if(j < result.size() && result[j] == -1) {
+                    result[j] = num;
+                    if(solve(i+1, n, result, used) == true) {
+                        return true;
+                    }
+                    result[j] = -1;
+                }
+            }
+
+            //UNDO
+            used[num] = false;
+            result[i] = -1;
+        }
+
+        return false;
+    }
+
+    vector<int> constructDistancedSequence(int n) {
+        vector<int> result(2*n-1, -1);
+
+        vector<bool> used(n+1, false);
+
+        solve(0, n, result, used);
+
+        return result;
+    }
+};
