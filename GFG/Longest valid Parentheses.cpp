51f0c0d13811d49b16b674c5fb10df843578226b
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-02-22 14:01:43 +0530

    Update Longest valid Parentheses.cpp

--- GFG/Longest valid Parentheses.cpp
+++ GFG/Longest valid Parentheses.cpp
@@ -76,3 +76,69 @@ class Solution {
         return ans;
     }
 };
+
+
+//Approach-2 : 
+class Solution {
+  public:
+    int maxLength(string& s) {
+        // code here
+        int n = s.length();
+        int open = 0, close = 0, ans = 0;
+
+        // Left to Right
+        for(int i = 0; i < n; i++){
+            if(S[i] == '(')
+                open++;
+            else
+                close++;
+
+            if(open == close)
+                ans = max(ans, open * 2);
+            else if(close > open)
+                open = close = 0;
+        }
+
+        // Right to Left
+        open = close = 0;
+        for(int i = n - 1; i >= 0; i--){
+            if(S[i] == '(')
+                open++;
+            else
+                close++;
+
+            if(open == close)
+                ans = max(ans, open * 2);
+            else if(open > close)
+                open = close = 0;
+        }
+
+        return ans;
+    }
+};
+
+
+//Approach-1 : 
+class Solution {
+  public:
+    int maxLength(string& s) {
+        // code here
+        stack<int> st;
+        st.push(-1);
+        for(int i = 0; i < s.length(); i++) {
+            if(s[i] == ')' && st.size() > 1 && s[st.top()] == '(') {
+                st.pop();
+            } else {
+                st.push(i);
+            }
+        }
+        int lim = s.length(), ans = 0;
+        while(!st.empty()) {
+            int p = st.top();
+            st.pop();
+            ans = max(ans, lim - p - 1);
+            lim = p;
+        }
+        return ans;
+    }
+};
