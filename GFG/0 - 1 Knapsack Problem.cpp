f597713e59f262bfded8f01aec174bd7a3fbb090
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-03-13 10:44:04 +0530

    Update 0 - 1 Knapsack Problem.cpp

--- GFG/0 - 1 Knapsack Problem.cpp
+++ GFG/0 - 1 Knapsack Problem.cpp
@@ -1,34 +1,31 @@
-//Approach-1
+//Approach-1 : 
 class Solution {
   public:
-  
     int solve(int i, int W, vector<int>& wt, vector<int>& val, vector<vector<int>>& dp) {
         if(i>=val.size()) return 0;
         if(dp[i][W]!=-1) return dp[i][W];
         int take=0;
-        if(W>=wt[i])
+        if(W-wt[i]>=0)
             take = val[i] + solve(i+1,W-wt[i],wt,val,dp);
         int not_take = solve(i+1,W,wt,val,dp);
         return dp[i][W] = max(take,not_take);
     }
     
-    // Function to return max value that can be put in knapsack of capacity W.
-    int knapSack(int W, vector<int>& wt, vector<int>& val) {
-        // Your code here
+    // Function to return max value that can be put in knapsack of capacity.
+    int knapsack(int W, vector<int> &val, vector<int> &wt) {
+        // code here
         vector<vector<int>> dp(val.size()+1, vector<int>(W+1,-1));
-        return solve(0,W,wt,val,dp);
+        return solve(0,W,wt,val,dp);        
     }
-
 };
 
 
-
-//Approach-2
+//Approach-2 : 
 class Solution {
   public:
-    // Function to return max value that can be put in knapsack of capacity W.
-    int knapSack(int W, vector<int>& wt, vector<int>& val) {
-        // Your code here
+    // Function to return max value that can be put in knapsack of capacity.
+    int knapsack(int W, vector<int> &val, vector<int> &wt) {
+        // code here
         int n = wt.size();
         vector<int> dp(W + 1, 0);
         
@@ -41,4 +38,3 @@ class Solution {
         return dp[W];
     }
 };
-
