b82f2f14879c688fe6c12aa9630365f5734f00bb
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-03-26 07:23:57 +0530

    Update Word Break.cpp

--- GFG/Word Break.cpp
+++ GFG/Word Break.cpp
@@ -1,3 +1,51 @@
+//Approach-1 : 
+class Solution {
+  public:
+    int wordBreak(string &s, vector<string> &dictionary) {
+        // code here
+        int n = s.size();
+        vector<bool> dp(n + 1, false);
+        dp[0] = true;  
+
+        for (int i = 0; i < n; i++) {
+            if (!dp[i])
+                continue;
+            for (const string &word : dictionary) {
+                int len = word.size();
+           
+                if (i + len <= n && s.substr(i, len) == word) {
+                    dp[i + len] = true;
+                }
+            }
+        }
+        return dp[n] ? 1 : 0;
+    }
+};
+
+
+//
+class Solution {
+  public:
+    int wordBreak(string &s, vector<string> &dictionary) {
+        // code here
+        int n=s.length();
+        vector<int>dp(n+1,0);
+        dp[0]=1;
+        for(int i=1;i<=n;i++){
+            for(auto it: dictionary){
+                int start= i- it.size();
+                if(start>=0 && dp[start] && s.substr(start, it.size()) == it){
+                    dp[i]=1;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
+
+
+//old version
 class Solution {
 public:
     bool solve(int idx,int n,string s,unordered_set<string> st)
