da383a5516830923ac7b129cb8d591c008324b46
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-04-04 09:43:44 +0530

    Create Lowest Common Ancestor in a Binary Tree.cpp

---
+++ Leetcode/Lowest Common Ancestor in a Binary Tree.cpp
@@ -0,0 +1,21 @@
+//Company Tags   : Accolite, Amazon, American Express, Expedia, MakeMyTrip, Microsoft, Payu, Snapdeal, Times Internet, Twitter
+//Leetcode Link  : https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root)
+            return NULL;
+        
+        if(root->val == p->val || root->val == q->val)
+            return root;
+        
+        TreeNode* l = lowestCommonAncestor(root->left, p, q);
+        TreeNode* r = lowestCommonAncestor(root->right, p, q);
+        
+        if(l && r)
+            return root;
+        
+        return l?l:r;
+    }
+};
