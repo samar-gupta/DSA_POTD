9f4313fafe175b6354f74155d1507e5ad2779389
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-04-13 06:10:09 +0530

    Update Clone an Undirected Graph.cpp

--- GFG/Clone an Undirected Graph.cpp
+++ GFG/Clone an Undirected Graph.cpp
@@ -1,3 +1,29 @@
+class Solution {
+  public:
+    unordered_map<Node*, Node*>mp;
+  
+    Node* cloneGraph(Node* node) {
+        // code here
+        if(node==NULL) return NULL;
+        if(mp.find(node)!=mp.end()){
+            return mp[node];
+        }
+        else{
+            Node* newNode=new Node(node->val);
+            mp[node]=newNode;
+            for(auto it:node->neighbors){
+                mp[node]->neighbors.push_back(cloneGraph(it));
+            }
+            
+        }
+        return mp[node];
+        
+    }
+};
+
+
+
+//old
 //Company Tags       : Google, Facebook, Amazon
 //Leetcode Qn Link   : https://leetcode.com/problems/clone-graph/
 //Gfg Qn Link        : https://www.geeksforgeeks.org/problems/clone-graph/1
