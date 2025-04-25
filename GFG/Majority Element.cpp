7f6b10ab93a96091dfc4dc9ede9262f4143325ca
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-04-25 18:23:49 +0530

    Create Majority Element.cpp

---
+++ GFG/Majority Element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int majorityElement(vector<int>& arr) {
+        // code here
+        int count=0, element=-1;
+        for(int num: arr){
+            if(count==0) element=num;
+            count += (num==element)?1:-1;
+        }
+        count=0;
+        for(int num: arr){
+            if(num==element){
+                count++;
+            }
+        }
+        if(count>arr.size()/2) return element;
+        return -1;
+    }
+};
