6ea07e9127adf61077ec9fa5e56d24dc8c51b1e2
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-04-27 15:43:17 +0530

    Update Multiply two strings.cpp

--- GFG/Multiply two strings.cpp
+++ GFG/Multiply two strings.cpp
@@ -3,38 +3,40 @@ class Solution {
     /*You are required to complete below function */
     string multiplyStrings(string& s1, string& s2) {
         // Your code here
-        if(s1 == "0" || s2 == "0"){
-           return "0";
-       }
-       int flag = 0;
-        if(s1[0]=='-'){
-            flag++;
-            s1 = s1.substr(1,s1.size()-1);
+        if (s1 =="0"|| s2 =="0")
+            return"0";
+        int sign =1;
+        if (s1[0] =='-') {
+            sign *= -1;
+            s1 = s1.substr(1);
         }
-          
-        if(s2[0]=='-'){
-            flag++;
-            s2 = s2.substr(1,s2.size()-1);
+        if (s2[0] =='-') {
+            sign *= -1;
+            s2 = s2.substr(1);
         }
-       vector<int> ans(s1.size()+s2.size(), 0);
-       for(int i=s1.size()-1 ; i>=0 ; i--){
-           for(int j=s2.size()-1 ; j>=0 ; j--){
-               ans[i+j+1] += (s1[i]-'0') * (s2[j] - '0');
-               ans[i+j] += ans[i+j+1]/10;
-               ans[i+j+1] %= 10;
-           }
-       }
-       int i=0;
-       while(i<ans.size() && ans[i] == 0){
-           i++;
-       }
-       string sol = "";
-       for(int j=i ; j<ans.size() ; j++){
-           sol.push_back(ans[j] + '0');
-       }
-       if(flag == 1){
-           sol = "-" + sol;
-       }
-       return sol;
+        
+        string ans(s1.length() + s2.length(), '0');
+        
+        for (int i =s2.length() -1; i >=0; i--) {
+            int carry =0;
+            for (int j =s1.length() -1; j >=0; j--) {
+                int product = (s1[j] -'0') * (s2[i] -'0') + (ans[i + j +1] -'0') + carry;
+                carry = product / 10;
+                ans[i + j +1] = (product %10) +'0';
+            }
+            ans[i] += carry;
+        }
+        
+        int start =0;
+        while (start <ans.length() -1&&ans[start] =='0') {
+            start++;
+        }
+        
+        string result = ans.substr(start);
+        if (sign ==-1&& result !="0") {
+            result = "-" + result;
+        }
+        
+        return result;
     }
 };
