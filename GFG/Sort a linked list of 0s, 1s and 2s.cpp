20bcaa9b9f78e45356a54c26a59852bf52ceee2b
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-04-29 17:26:36 +0530

    Create Sort a linked list of 0s, 1s and 2s.cpp

---
+++ GFG/Sort a linked list of 0s, 1s and 2s.cpp
@@ -0,0 +1,33 @@
+class Solution {
+  public:
+    Node* segregate(Node* head) {
+        // code here
+        Node* temp=head;
+        int zero=0, one=0, two=0;
+        while(temp){
+            if(temp->data==0) zero++;
+            else if(temp->data==1) one++;
+            else two++;
+            
+            temp=temp->next;
+        }
+        
+        temp=head;
+        while(zero){
+            temp->data=0;
+            zero--;
+            temp=temp->next;
+        }
+        while(one){
+            temp->data=1;
+            one--;
+            temp=temp->next;
+        }
+        while(two){
+            temp->data=2;
+            two--;
+            temp=temp->next;
+        }
+        return head;
+    }
+};
