9bb99cb06168cc3ad697a69997b4825fe40bba37
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-05-05 10:40:57 +0530

    Create Search in an almost Sorted Array.cpp

---
+++ GFG/Search in an almost Sorted Array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int findTarget(vector<int>& arr, int target) {
+        // code here
+        int n = arr.size();
+        int l = 0, r = n-1;
+        
+        while(l <= r){
+            int mid = (l+r)/2;
+            if(arr[mid] == target) return mid;
+            else if(arr[mid] > target){
+                if(mid+1 < n && arr[mid+1] == target) return mid+1;
+                else r = mid-1;
+            }
+            else{
+                if(mid > 0 && arr[mid-1] == target) return mid-1;
+                else l = mid+1;
+            }
+        }
+        
+        return -1;
+    }
+};
