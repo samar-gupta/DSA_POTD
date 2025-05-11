517964053065e8dc89bfcfc658b86b4f9d420ef8
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-05-11 13:30:34 +0530

    Update Three Consecutive Odds.cpp

--- Leetcode/Three Consecutive Odds.cpp
+++ Leetcode/Three Consecutive Odds.cpp
@@ -1,65 +1,44 @@
-//Leetcode Link : https://leetcode.com/problems/three-consecutive-odds/?envType=daily-question&envId=2024-07-01
+//Leetcode Link : https://leetcode.com/problems/three-consecutive-odds
 
-//Approach-1 
+//Approach-1 (Iterating and checking consecutive 3 elements)
 //T.C : O(n)
 //S.C : O(1)
 class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr) {
-        int count = 0;
+        //Approach-1
         int n = arr.size();
         
-        for(int i = 0; i < n; i++){
-            if(arr[i] % 2 == 1) {
-                count++;    //Count it
-            } else {
-                count = 0;   //Reset it
+        for(int i = 0; i < n-2; i++) {
+            if(arr[i]%2 && arr[i+1]%2 && arr[i+2]%2) {    //clean version of arr[i]%2 == 1
+                return true;
             }
-            
-            if(count == 3)
-                return true;
-        }  
-        
-        return false;       
-    }
-};
-
-
-
-//Approach-2
-//T.C : O(n)
-//S.C : O(1)
-class Solution {
-public:
-    bool threeConsecutiveOdds(vector<int>& arr) {
-        int n = arr.size();
-        
-        for(int i = 0; i < n-2; i++){
-            if(arr[i] % 2 == 1 && arr[i+1] % 2 == 1 && arr[i+2] % 2 == 1) {
-                return true;
-            } 
         }
-        
         return false;
     }
 };
 
 
-
-//Approach-3
+//Approach-2 (Iterating checking count of odd elements)
 //T.C : O(n)
 //S.C : O(1)
 class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr) {
+        //Approach-2
         int n = arr.size();
-        
-        for(int i = 0; i < n-2; i++){
-            if((arr[i] * arr[i+1] * arr[i+2]) % 2 == 1) {
+        int oddCount = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(arr[i] % 2 == 1) {
+                oddCount++;
+            } else {
+                oddCount = 0;
+            }
+
+            if(oddCount == 3)
                 return true;
-            } 
         }
-        
         return false;
     }
 };
