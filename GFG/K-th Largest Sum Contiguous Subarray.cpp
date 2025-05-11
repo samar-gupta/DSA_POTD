780535c873b236690019b16d1f99c16a3b9e3062
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-05-11 13:19:40 +0530

    Update K-th Largest Sum Contiguous Subarray.cpp

--- GFG/K-th Largest Sum Contiguous Subarray.cpp
+++ GFG/K-th Largest Sum Contiguous Subarray.cpp
@@ -2,17 +2,26 @@ class Solution {
   public:
     int kthLargest(vector<int> &arr, int k) {
         // code here
-        priority_queue<int,vector<int>,greater<int>>pq;
-	    for(int i=0;i<n;i++) {
-	        int sum = 0;
-	        for(int j=i;j<n;j++) {
-	            sum += arr[j];
-	            pq.push(sum);
-	            if(pq.size()>k) {
-	                pq.pop();
-	            }
-	        }
+        int n = arr.size();
+        
+        priority_queue <int, vector <int>, greater<int>> pq;
+        
+        for(int i = 0; i < n; i++){
+            int sum = 0;
+            for(int j = i; j < n; j++){
+                sum += arr[j];
+                
+                if(pq.size() < k) pq.push(sum);
+                else{
+                    int top = pq.top();
+                    if(sum > top){
+                        pq.pop();
+                        pq.push(sum);
+                    }
+                }
+            }
         }
+        
         return pq.top();
     }
 };
