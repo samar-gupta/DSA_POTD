0f22aa93f490f6106b0a0b1aae122b58141eb611
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-05-13 11:06:28 +0530

    Create Total Characters in String After Transformations I.cpp

---
+++ Leetcode/Total Characters in String After Transformations I.cpp
@@ -0,0 +1,44 @@
+//Leetcode Link : https://leetcode.com/problems/total-characters-in-string-after-transformations-i
+
+//Approach - Using frequency count in map
+//T.C : O(n+t)
+//S.C : O(26) ~= O(1)
+class Solution {
+public: 
+    int M = 1e9+7;
+    int lengthAfterTransformations(string s, int t) {
+        int n = s.length();
+        //unordered_map<char, int> mp;
+        vector<int> mp(26, 0);
+
+        for(char &ch : s) {
+            mp[ch - 'a']++;
+        }
+
+        for(int count = 1; count <= t; count++) { //O(t)
+            //unordered_map<char, int> temp;
+            vector<int> temp(26, 0);
+
+            for(int i = 0; i < 26; i++) { //O(26)
+                char ch  = i + 'a'; // 0->a, 1->b and so on
+                int freq = mp[i];
+
+                if(ch != 'z') {
+                    temp[(ch+1) - 'a'] = (temp[(ch+1) - 'a'] + freq) % M;
+                } else {
+                    temp['a' - 'a'] = (temp['a' - 'a'] + freq) % M;
+                    temp['b' - 'a'] = (temp['b' - 'a'] + freq) % M;
+                }
+            }
+
+            mp = move(temp);
+        }
+
+        int result = 0;
+        for(int i = 0; i < 26; i++) {
+            result = (result + mp[i]) % M;
+        }
+
+        return result;
+    }
+};
