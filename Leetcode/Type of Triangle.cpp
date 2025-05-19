c92577883cb66527d05901af16b1138fc4320e15
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-05-19 09:31:33 +0530

    Create Type of Triangle.cpp

---
+++ Leetcode/Type of Triangle.cpp
@@ -0,0 +1,25 @@
+//Leetcode Link : https://leetcode.com/problems/type-of-triangle
+
+//Approach (Simply checking triangle properties)
+//T.C : O(1) 
+//S.C : O(1)
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        bool valid = (nums[0] + nums[1] > nums[2]) &&
+                     (nums[0] + nums[2] > nums[1]) &&
+                     (nums[1] + nums[2] > nums[0]);
+
+        if(!valid) {
+            return "none";
+        }
+
+        if(nums[0] == nums[1] && nums[1] == nums[2]) {
+            return "equilateral";
+        } else if(nums[0] != nums[1] && nums[0] != nums[2] && nums[1] != nums[2]) {
+            return "scalene";
+        }
+
+        return "isosceles";
+    }
+};
