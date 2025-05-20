3c6decb355b89dd6d97e66e0d4e91c7a8028a130
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-05-20 18:00:36 +0530

    Update Burning Tree.cpp

--- GFG/Burning Tree.cpp
+++ GFG/Burning Tree.cpp
@@ -1,4 +1,55 @@
-//Approach-1 (Convert tree to graph)
+//updated
+//Approach : 
+class Solution {
+  public:
+    Node* key = nullptr;    //target node
+    void inorder(Node* root, int& target, unordered_map<Node*, Node*>& mp) {
+        if (!root) return;
+        if (root->left) {
+            mp[root->left] = root;
+            inorder(root->left, target, mp);
+        }
+        if (root->data == target) key = root;
+        if (root->right) {
+            mp[root->right] = root;
+            inorder(root->right, target, mp);
+        }
+    }
+    int minTime(Node* root, int target) {
+        // code here
+        unordered_map<Node*, Node*> mp;    //parent
+        unordered_map<Node*, bool> vis;
+        inorder(root, target, mp);       //inorder to store parent
+        int t = -1;         //time counter
+        queue<Node*> q;
+        q.push(key);
+        vis[key] = true;
+        while (!q.empty()) {
+            int size = q.size();
+            while (size--) {
+                Node* node = q.front();
+                q.pop();
+                if (node->left && !vis[node->left]) {
+                    vis[node->left] = true;
+                    q.push(node->left);
+                }
+                if (node->right && !vis[node->right]) {
+                    vis[node->right] = true;
+                    q.push(node->right);
+                }
+                if (mp[node] && !vis[mp[node]]) {       //root node doesn't have parent and will give segmentation error if we don't write mp[node] along with !visited[mp[node]]
+                    vis[mp[node]] = true;
+                    q.push(mp[node]);
+                }
+            } t++;
+        } return t;
+    }
+};
+
+
+
+//old
+//Approach-1 : (Convert tree to graph)
 //T.C : O(n) - visiting all nodes
 //S.C : O(n) - storing all nodes in graph
 class Solution {
