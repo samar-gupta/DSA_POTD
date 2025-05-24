ca766f6861c8366c659306809208fe47a517b9ef
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-05-24 18:24:59 +0530

    Update Sum of all substrings of a number.cpp

--- GFG/Sum of all substrings of a number.cpp
+++ GFG/Sum of all substrings of a number.cpp
@@ -1,21 +1,46 @@
-class Solution
-{
-    public:
-    //Function to find sum of all possible substrings of the given string.
-    long long sumSubstrings(string s){
-        
-        // your code here
-        long  long prev=0, curr=0, ans=0;
-        long long mod=1e9+7;
-        
-        for(int i=0;i<s.length();i++){
-            curr=(s[i]-'0')*(i+1) + prev*10;
-            curr%=mod;
-            ans+=curr;
-            ans%=mod;
-            prev=curr;
+//Approach-1 : 
+//T.C : O(n)
+//S.C : O(n)
+/*
+   dp[i] = sum of all substrings upto index i such that index i is included
+   final ans = dp[1] + dp[2] + .... dp[n-1] + dp[n]
+*/  
+class Solution {
+  public:
+    int sumSubstrings(string &s) {
+        // code here
+        int n = s.size();
+       vector<int> dp(n+1,0); 
+       int ans = dp[0];
+       for(int i = 1;i<=n;i++)
+       {
+           dp[i] = (dp[i-1]*10) + (s[i-1] - '0')*(i);
+           ans += dp[i];
+           
+       }
+       //int ans = accumulate(dp.begin(), dp.end(),0);
+       return ans;
+    }
+};
+
+
+//Approach-2 : 
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+  public:
+    int sumSubstrings(string &s) {
+        // code here
+        int n = s.length();
+        int result = 0;
+        int prev = 0;
+
+        for (int i = 0; i < n; ++i) {
+            int digit = s[i] - '0';
+            prev = (prev * 10 + (i + 1) * digit); 
+            result += prev;
         }
-        
-        return ans;
+
+        return result;
     }
 };
