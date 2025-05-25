bbc927516659f69ed9c8bfd6904fa0457ba6b11c
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-05-25 18:38:43 +0530

    Create Longest Palindrome by Concatenating Two Letter Words.cpp

---
+++ Leetcode/Longest Palindrome by Concatenating Two Letter Words.cpp
@@ -0,0 +1,38 @@
+//Leetcode Link : https://leetcode.com/problems/longest-palindrome-by-concatenating-two-letter-words
+
+//Approach - Using map
+//T.C : O(n), where n = number of words
+//S.C : O(n)
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        unordered_map<string, int> mp;
+
+        int result = 0;
+
+        for(string &word : words) {
+            string reversedWord = word;
+            swap(reversedWord[0], reversedWord[1]);
+
+            if(mp[reversedWord] > 0) {
+                result += 4;
+                mp[reversedWord]--;
+            } else {
+                mp[word]++;
+            }
+        }
+
+        //Check equal character words. Use only one
+        for(auto &it : mp) {
+            string word = it.first;
+            int count = it.second;
+
+            if(word[0] == word[1] && count > 0) {
+                result += 2;
+                break;
+            }
+        }
+
+        return result;
+    }
+};
