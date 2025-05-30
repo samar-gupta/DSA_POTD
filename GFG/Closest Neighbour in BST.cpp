aedfe8c3b50bf69c8445bda84625fbf6ce96e32d
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-05-30 07:30:45 +0530

    Update Closest Neighbour in BST.cpp

--- GFG/Closest Neighbour in BST.cpp
+++ GFG/Closest Neighbour in BST.cpp
@@ -1,3 +1,27 @@
+//updated
+class Solution {
+  public:
+    int findMaxFork(Node* root, int k) {
+        // code here
+        int ans = -1;
+        Node* curr = root;
+        
+        while(curr){
+            
+            if(curr->data <= k){
+                ans = curr->data;
+                curr = curr->right;
+            }else{
+                curr = curr->left;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
+//old
 class Solution {
   public:
     int ans=INT_MIN;
