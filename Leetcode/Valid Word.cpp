56338458804b42ccfaf585dbf2898db176b36e3f
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-07-15 07:20:29 +0530

    Create Valid Word.cpp

---
+++ Leetcode/Valid Word.cpp
@@ -0,0 +1,31 @@
+//Leetcode Link : https://leetcode.com/problems/valid-word
+
+//Approach (simple traverse and check)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    bool isValid(string word) {
+        if(word.length() < 3) {
+            return false;
+        }
+
+        bool hasVowel     = false;
+        bool hasConsonant = false;
+
+        for(char ch : word) {
+            if(isalpha(ch)) {
+                ch = tolower(ch);
+                if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            } else if(!isdigit(ch)) {
+                return false;
+            }
+        }
+
+        return hasVowel && hasConsonant;
+    }
+};
