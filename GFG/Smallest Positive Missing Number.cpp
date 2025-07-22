d0552bb93734eac94173288c6036e909566baaf7
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-07-22 15:34:40 +0530

    Update Smallest Positive Missing Number.cpp

--- GFG/Smallest Positive Missing Number.cpp
+++ GFG/Smallest Positive Missing Number.cpp
@@ -1,3 +1,28 @@
+//updated
+//Smallest Positive Missing
+class Solution {
+  public:
+    int missingNumber(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        
+        for(int i = 0;i<n;i++){
+            while(arr[i]>0 && arr[i]<=n  && arr[arr[i] - 1] != arr[i]){
+                swap(arr[i],arr[arr[i]-1]);
+            }
+        }
+        
+        for(int i =0 ;i<n;i++){
+            if(arr[i]!=i+1){
+                return i+1;
+            }
+        }
+        return n+1;
+    }
+};
+
+
+//old
 //Approach-1 : 
 //T.C : O(nlogn)
 //S.C : O(1)
