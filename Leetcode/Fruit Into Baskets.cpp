10303b0efe5f86e9b76a9c3cdcc699435a5d1d55
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-08-04 17:42:13 +0530

    Create Fruit Into Baskets.cpp

---
+++ Leetcode/Fruit Into Baskets.cpp
@@ -0,0 +1,33 @@
+//Leetcode Link : https://leetcode.com/problems/fruit-into-baskets/description/
+
+//Approach (Sliding Window using map)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+
+        unordered_map<int, int> mp;
+
+        int i = 0;
+        int j = 0;
+        int count = 0;
+
+        while(j < n) {
+            mp[fruits[j]]++;
+
+            if(mp.size() <= 2) {
+                count = max(count, j-i+1);
+            } else {
+                mp[fruits[i]]--;
+                if(mp[fruits[i]] == 0)
+                    mp.erase(fruits[i]);
+                i++;
+            }
+
+            j++;
+        }
+        return count;
+    }
+};
