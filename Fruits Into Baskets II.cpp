53489e3223b9a468a0d3770849931c6f81e81256
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-08-05 20:23:29 +0530

    Create Fruits Into Baskets II.cpp

---
+++ Fruits Into Baskets II.cpp
@@ -0,0 +1,21 @@
+//Leetcode Link : https://leetcode.com/problems/fruits-into-baskets-ii/
+
+//Approach-1 : (Using greedy approach)
+//T.C : O(n^2)
+//S.C : O(1)
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int n = fruits.size(), alloted = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (fruits[i] <= baskets[j]) {
+                    baskets[j] = -1; // mark basket as used
+                    alloted++;
+                    break;
+                }
+            }
+        }
+        return n - alloted;
+    }
+};
