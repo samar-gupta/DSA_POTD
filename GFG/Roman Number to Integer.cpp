d01c449304cda19f6bf2dcb1e7f9a7a60f11de48
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-08-06 18:10:56 +0530

    Update Roman Number to Integer.cpp

--- GFG/Roman Number to Integer.cpp
+++ GFG/Roman Number to Integer.cpp
@@ -1,3 +1,76 @@
+//Approach-1 : (Using Hashmap)
+/*Intuition - In Roman numerals, if a smaller numeral comes before a larger numeral, it is subtracted, 
+              otherwise, they are added to get the integer value. 
+    eg:  IV => I (1) is less than V (5) => 5-1 = 4
+         VI => V(5) is more than I (1) => 5+1 = 6
+*/
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+  public:
+    int romanToDecimal(string &s) {
+       // code here
+       int n = s.length();
+       int ans=0;
+       unordered_map<char,int> num;
+       num['I'] = 1;
+       num['V'] = 5;
+       num['X'] = 10;
+       num['L'] = 50;
+       num['C'] = 100;
+       num['D'] = 500;
+       num['M'] = 1000;
+       
+       for(int i=n-1;i>=0;i--){
+           if(i<n-1 && num[s[i]]<num[s[i+1]])
+               ans-=num[s[i]];
+           else{
+               ans+=num[s[i]];
+           }
+       }
+       
+       return ans;
+    }
+};
+
+
+//Approach-2 : 
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+  public:
+  
+    int value(char ch){
+        if(ch == 'I') return (1);
+        if(ch == 'V') return (5);
+        if(ch == 'X') return (10);
+        if(ch == 'L') return (50);
+        if(ch == 'C') return (100);
+        if(ch == 'D') return (500);
+        if(ch == 'M') return (1000);
+        return (0);
+    }
+    
+    int romanToDecimal(string &s) {
+        // code here
+        int ans = 0;
+        int n = s.length();
+        for(int i = 0; i < n; i++){
+            if(i < n-1 && value(s[i]) < value(s[i+1])){
+                ans += value(s[i+1]) - value(s[i]);
+                i++;
+            }
+            else{
+                ans += value(s[i]);
+            }
+        }
+        return (ans);
+    }
+};
+
+
+
+//old
 class Solution {
   public:
     int romanToDecimal(string &s) {
