84482ace221eda826dbc81e117f163f340a48440
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-08-06 18:53:26 +0530

    Update Fruits Into Baskets II.cpp

--- Leetcode/Fruits Into Baskets II.cpp
+++ Leetcode/Fruits Into Baskets II.cpp
@@ -1,5 +1,33 @@
 //Leetcode Link : https://leetcode.com/problems/fruits-into-baskets-ii/
 
+//Approach-1 : 
+//T.C : O(n^2)
+//S.C : O(1)
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int unplaced = 0;
+        int n = fruits.size();
+
+        for(int i = 0; i < n; i++) {
+            bool placed = false;
+            for(int j = 0; j < n; j++) {
+                if(fruits[i] <= baskets[j]) {
+                    placed = true;
+                    baskets[j] = 0;
+                    break;
+                }
+            }
+            if(!placed) {
+                unplaced++;
+            }
+        }
+
+        return unplaced;
+    }
+};
+
+
 //Approach-1 : (Using greedy approach)
 //T.C : O(n^2)
 //S.C : O(1)
