97d46bf8693194801afdbc629606542244e7c1d7
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-08-08 13:38:16 +0530

    Update Longest Prefix Suffix.cpp

--- GFG/Longest Prefix Suffix.cpp
+++ GFG/Longest Prefix Suffix.cpp
@@ -1,3 +1,35 @@
+//Approach-1 : (Using Two Pointers)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+  public:
+    int getLPSLength(string &str) {
+        // code here
+        int p = 0;  // Prefix pointer
+        int s = 1;  // Suffix pointer
+        int cnt = 0;  // Count of matching prefix-suffix
+        int pos = 1;  // Starting position of suffix
+        int n = str.size();  // Length of the string
+
+        while(p < n && s < n) {
+            if(str[p] == str[s]) {
+                cnt++;
+                p++;
+                s++;
+            } else {
+                cnt = 0;
+                pos++;
+                s = pos;
+                p = 0;
+            }
+        }
+
+        return cnt;
+    }
+};
+
+
+//old
 class Solution {
   public:
     int lps(string str) {
