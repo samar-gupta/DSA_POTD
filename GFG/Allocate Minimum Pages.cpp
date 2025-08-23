ebdb3ecd9afa352f4d851ce8045616730391c04a
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-08-23 07:13:06 +0530

    Update Allocate Minimum Pages.cpp

--- GFG/Allocate Minimum Pages.cpp
+++ GFG/Allocate Minimum Pages.cpp
@@ -1,3 +1,56 @@
+//Approach-1 : (Using Binary Search on Ans)
+//T.C : O(nlogn)
+//S.C : O(1)
+class Solution {
+  public:
+    bool isPossible(vector<int>& arr, int n, int k, int maxPages) {
+
+        int count = 1;
+
+        int currSum = 0;
+
+        for (int pages : arr) {
+            if (currSum + pages > maxPages) {
+                count++;
+                currSum = pages;
+                if (count > k) return false;
+            } else {
+                currSum += pages;
+            }
+        }
+        return true;
+    }
+    
+    int findPages(vector<int> &arr, int k) {
+        // code here
+        int n = arr.size();
+
+        if (k > n) return -1;
+
+
+        int low = 0, high = 0;
+        for (int pages : arr) {
+            low = max(low, pages);
+            high += pages;
+        }
+
+        int ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (isPossible(arr, n, k, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
+
+
+//old
 class Solution {
   public:
   bool isPossible(vector<int>&arr,int k,int mid){
