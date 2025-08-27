8af4551270f0bf2395aae421ff014352d9ad4a94
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-08-27 08:57:13 +0530

    Update Count the number of possible triangles.cpp

--- GFG/Count the number of possible triangles.cpp
+++ GFG/Count the number of possible triangles.cpp
@@ -1,3 +1,33 @@
+//Approach-1 : (Sorting)
+//T.C : O(nlogn) + O(n^2)
+//S.C : O(1)
+class Solution {
+  public:
+    int countTriangles(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        if (n < 3) return 0;
+
+        sort(arr.begin(), arr.end());
+        int res = 0;
+
+        for (int k = n - 1; k >= 2; k--) {
+            int i = 0, j = k - 1;
+            while (i < j) {
+                if (arr[i] + arr[j] > arr[k]) {
+                    res += (j - i);
+                    j--;
+                } else {
+                    i++;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+
+//old
 class Solution {
   public:
     // Function to count the number of possible triangles.
