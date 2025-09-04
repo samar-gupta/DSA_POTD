9462ac60c2bca146e98dbe8471351df7527dfe64
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-09-04 18:04:18 +0530

    Update Linked List Group Reverse.cpp

--- GFG/Linked List Group Reverse.cpp
+++ GFG/Linked List Group Reverse.cpp
@@ -1,3 +1,87 @@
+//Approach-1 : (Using Recursion)
+class Solution {
+  public:
+    Node* reverse(Node* head, Node* stop) {
+        Node* prev = stop;
+        while (head != stop) {
+            Node* next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+    
+    Node *reverseKGroup(Node *head, int k) {
+        // code here
+        Node* curr = head;
+     
+        for (int i = 0; i < k; i++) {
+            if (curr == nullptr) 
+                return reverse(head, nullptr);
+            curr = curr->next;
+        }
+
+        Node* newHead = reverse(head, curr);
+        head->next = reverseKGroup(curr, k);
+        return newHead;
+    }
+};
+
+
+//Approach-2 : (Interative)
+class Solution {
+  public:
+    void reverse(Node* head, Node* stop) {
+        Node* prev = stop;
+        while (head != stop) {
+            Node* nxt = head->next;
+            head->next = prev;
+            prev = head;
+            head = nxt;
+        }
+    }
+    
+    Node *reverseKGroup(Node *head, int k) {
+        // code here
+        if (k <= 1) return head;
+
+        Node dummy(0);
+        dummy.next = head;
+        Node* prev = &dummy;
+
+        while (true) {
+            
+            Node* end = prev;
+            int count = 0;
+            
+            while (count < k && end->next != nullptr) {
+                end = end->next;
+                count++;
+            }
+
+            if (count == 0) break;  
+
+            Node* start = prev->next;
+            Node* next = end->next;
+
+            reverse(start, next);   
+
+            prev->next = end;      
+            start->next = next;     
+
+            prev = start;          
+
+            if (count < k) break;    
+        }
+
+        return dummy.next;
+    }
+};
+
+
+
+//old
 class Solution {
   public:
     Node *reverseKGroup(Node *head, int k) {
