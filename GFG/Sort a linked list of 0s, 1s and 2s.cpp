1af7d868960ba23b09e61efebdc2264572f01b47
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-09-05 14:51:48 +0530

    Update Sort a linked list of 0s, 1s and 2s.cpp

--- GFG/Sort a linked list of 0s, 1s and 2s.cpp
+++ GFG/Sort a linked list of 0s, 1s and 2s.cpp
@@ -1,3 +1,80 @@
+//Approach-1 : (Using array to store frequency)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+  public:
+    Node* segregate(Node* head) {
+        // code here
+        if (head == nullptr) return head;
+        int count[3] = {0, 0, 0};
+
+        Node* temp = head;
+        while (temp != nullptr) {
+            count[temp->data]++;
+            temp = temp->next;
+        }
+
+        temp = head;
+        for (int i = 0; i < 3; i++) {
+            while (count[i]-- > 0) {
+                temp->data = i;
+                temp = temp->next;
+            }
+        }
+        return head;
+    }
+};
+
+
+//Approach-2 : (3 Pointer) (Good)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+  public:
+    Node* segregate(Node* head) {
+        // code here
+        if (head == nullptr || head->next == nullptr) return head;
+
+        Node* zeroDummy = new Node(0);
+        Node* oneDummy  = new Node(0);
+        Node* twoDummy  = new Node(0);
+
+        Node* zero = zeroDummy;
+        Node* one  = oneDummy;
+        Node* two  = twoDummy;
+
+        Node* curr = head;
+        while (curr != nullptr) {
+            if (curr->data == 0) {
+                zero->next = curr;
+                zero = zero->next;
+            } else if (curr->data == 1) {
+                one->next = curr;
+                one = one->next;
+            } else { 
+                two->next = curr;
+                two = two->next;
+            }
+            curr = curr->next;
+        }
+
+        zero->next = (oneDummy->next != nullptr) ? oneDummy->next : twoDummy->next;
+        one->next  = twoDummy->next;
+        two->next  = nullptr;
+
+        Node* newHead = zeroDummy->next;
+
+        delete zeroDummy;
+        delete oneDummy;
+        delete twoDummy;
+
+        return newHead;
+    }
+};
+
+
+
+//old
 class Solution {
   public:
     Node* segregate(Node* head) {
