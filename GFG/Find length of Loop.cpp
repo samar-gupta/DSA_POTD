334a7ec8e74de340800632ed02cf0f743ad5b58b
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-09-06 17:12:31 +0530

    Update Find length of Loop.cpp

--- GFG/Find length of Loop.cpp
+++ GFG/Find length of Loop.cpp
@@ -1,3 +1,38 @@
+//Approach-1 : (Using slow and fast pointer)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+  public:
+    int countLoopNodes(Node* loopNode) {
+        int count = 1;
+        Node* temp = loopNode->next;
+
+        while (temp != loopNode) {
+            count++;
+            temp = temp->next;
+        }
+        return count;
+    }
+    
+    int lengthOfLoop(Node *head) {
+        // code here
+        Node* slow = head;
+        Node* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                return countLoopNodes(slow);
+            }
+        }
+        return 0;
+    }
+};
+
+
+//old
 class Solution {
   public:
     // Function to find the length of a loop in the linked list.
