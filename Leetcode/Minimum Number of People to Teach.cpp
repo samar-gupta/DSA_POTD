f67e7fd3de14d561703a259636690438f88db2d7
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-09-10 17:24:34 +0530

    Create Minimum Number of People to Teach.cpp

---
+++ Leetcode/Minimum Number of People to Teach.cpp
@@ -0,0 +1,44 @@
+//Leetcode Link : https://leetcode.com/problems/minimum-number-of-people-to-teach
+
+//Approach (Greedily picking the mostKnown language among the sadUsers (those who cannpt talk to their friends))
+//T.C : O(m*n)
+//S.C : O(m+n)
+class Solution {
+public:
+    int minimumTeachings(int n, vector<vector<int>>& languages, vector<vector<int>>& friendships) {
+        unordered_set<int> sadUsers;
+
+        for(auto &friends : friendships) {
+            int u = friends[0] - 1; //convert this to - based indexing
+            int v = friends[1] - 1;
+
+            unordered_set<int> langSet(begin(languages[u]), end(languages[u]));
+            bool canTalk = false;
+            for(int lang : languages[v]) {
+                if(langSet.count(lang)) {
+                    canTalk = true;
+                    break;
+                }
+            }
+
+            if(!canTalk) {
+                sadUsers.insert(u);
+                sadUsers.insert(v);
+            }
+        }
+
+        //Find max known language among sad users
+        vector<int> language(n+1, 0);
+        int mostKnownLang = 0;
+
+        for(int user : sadUsers) {
+            for(int lang : languages[user]) {
+                language[lang]++;
+                mostKnownLang = max(mostKnownLang, language[lang]);
+            }
+        }
+
+        return sadUsers.size() - mostKnownLang;
+        
+    }
+};
