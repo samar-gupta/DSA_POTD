ec33bc727665125771cecfdcbd0e59c4c7c2f904
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-10-01 16:32:29 +0530

    Update All Unique Permutations of an array.cpp

--- GFG/All Unique Permutations of an array.cpp
+++ GFG/All Unique Permutations of an array.cpp
@@ -1,3 +1,89 @@
+//Approach-1 : (Using Recursion)
+class Solution {
+  public:
+    void helper(vector<int> &A, int curr, int n,set<vector<int>> &s){
+        if(curr>=n){
+            s.insert(A);
+            return;
+        }
+        for(int i=curr;i<n;i++){
+            swap(A[curr],A[i]);     //swap
+            helper(A,curr+1,n,s);  //deal with it
+            swap(A[curr],A[i]);   //return back to previous state
+        }
+    }
+    
+    vector<vector<int>> uniquePerms(vector<int>& A) {
+        // code here
+        int n = A.size();
+        set<vector<int>> s;
+        vector<vector<int>> res;
+        helper(A,0,n,s);
+        for(auto itr: s){
+            res.push_back(itr);
+        }
+        return res;
+    }
+};
+
+
+//Approach-1 : (Using recursion)
+class Solution {
+  public:
+  
+    set<vector<int>>st;
+    
+    void solve (int index, vector<int> &arr , int n){
+        // Base Case : if the current index reached n then insert it to the set
+        if(index == n) {
+            st.insert(arr);
+            return ;
+        }
+        
+        for(int i=index; i<n; i++){
+            // avoiding duplicate elements
+            if(i!= index && arr[index] == arr[i])
+                continue;
+            swap(arr[i], arr[index] );
+            // calling the recursive function
+            solve(index+1, arr , n);
+            // backtracking
+            swap(arr[i], arr[index] );
+        }
+    }
+    
+    vector<vector<int>> uniquePerms(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        // to avoid duplicate values
+        sort(arr.begin(), arr.end());
+        // calling the recursive function
+        solve(0, arr , n);
+       
+        // inserting all permutaions in set into the answer vector
+        vector<vector<int>>ans(st.begin(), st.end());
+        return ans;
+    }
+};
+
+
+//Approach-2 : (Using using next_permutation inbuild function)
+//next_permutaion is an funtion that creates the permutation automatically in the greater lexicographic way.
+class Solution {
+  public:
+    vector<vector<int>> uniquePerms(vector<int>& arr) {
+        // code here
+        vector<vector<int>>ans;
+        sort(arr.begin(), arr.end());
+        ans.push_back(arr);
+        while(next_permutation(arr.begin() , arr.end())) ans.push_back(arr); 
+        return ans;
+    }
+};
+
+
+
+//old
 class Solution {
   public:
     vector<vector<int>> uniquePerms(vector<int> &arr ,int n) {
