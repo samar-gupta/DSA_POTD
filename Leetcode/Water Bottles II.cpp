2c12392cd9b2f1d13fe36ac0b3155a97303b31d6
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-10-03 15:40:55 +0530

    Update Water Bottles II.cpp

--- Leetcode/Water Bottles II.cpp
+++ Leetcode/Water Bottles II.cpp
@@ -1,5 +1,42 @@
 //Leetcode Link : https://leetcode.com/problems/water-bottles-ii/description/
 
+//Approach-1 Simple Simulation
+//T.C : O(root(numBottles))
+//S.C : O(1)
+class Solution {
+public:
+    int maxBottlesDrunk(int numBottles, int numExchange) {
+        int empty = numBottles;
+
+        int drank = numBottles;
+
+        while(empty >= numExchange) {
+            empty -= numExchange;
+
+            drank++;
+
+            empty++;
+
+            numExchange++;
+        }
+
+        return drank;
+    }
+};
+
+
+//Approach-2 (Maths derivation - Using Sridharacharya formula)
+//T.C : O(1)
+//S.C : O(1)
+class Solution {
+public:
+    int maxBottlesDrunk(int numBottles, int numExchange) {
+        return numBottles + ((-2*numExchange + 3 + sqrt(4*numExchange*numExchange+8*numBottles - 12 * numExchange + 1))/2);
+    }
+};
+
+
+
 //Approach-1 :
 /*
 Intuition : 
