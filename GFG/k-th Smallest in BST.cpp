25dfa8962d8321c03ffc6828ead1b57d5676336d
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-10-15 16:24:23 +0530

    Update k-th Smallest in BST.cpp

--- GFG/k-th Smallest in BST.cpp
+++ GFG/k-th Smallest in BST.cpp
@@ -1,3 +1,6 @@
+//Approach-1 : 
+//Since inorder traversal of a BST gives us ascending order traversal of a tree, 
+//so we used inorder traversal and thus decreased K in that order
 class Solution {
   public:
     void inorder(Node* root, int &k, int &ans)
@@ -15,10 +18,10 @@ class Solution {
         
         inorder(root->right, k, ans);
     }
-    
+
     // Return the Kth smallest element in the given BST
     int kthSmallest(Node *root, int k) {
-        // add code here.
+        // code here
         int ans = -1;
         inorder(root, k, ans);
         return ans;
