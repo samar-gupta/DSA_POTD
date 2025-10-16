7ab12c82fe5c0d1d62227a21325878f087bebf63
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-10-16 15:41:39 +0530

    Update Smallest Missing Non-negative Integer After Operations.cpp

--- Leetcode/Smallest Missing Non-negative Integer After Operations.cpp
+++ Leetcode/Smallest Missing Non-negative Integer After Operations.cpp
@@ -1,5 +1,6 @@
 //Leetcode Link : https://leetcode.com/problems/smallest-missing-non-negative-integer-after-operations
 
+//NOTE : Adding value to any number doesn’t change its remainder modulo value, so what really matters is the frequency of each remainder when the numbers are divided by value.
 //Approach - Observation about Modulo(%) values
 //T.C : O(n)
 //S.C : O(n)
