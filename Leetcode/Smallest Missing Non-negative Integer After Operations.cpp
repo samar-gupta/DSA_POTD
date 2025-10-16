b720cbac64a6f8757fdc182a5cdc8c4893100ac2
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-10-16 15:39:39 +0530

    Create Smallest Missing Non-negative Integer After Operations.cpp

---
+++ Leetcode/Smallest Missing Non-negative Integer After Operations.cpp
@@ -0,0 +1,27 @@
+//Leetcode Link : https://leetcode.com/problems/smallest-missing-non-negative-integer-after-operations
+
+//Approach - Observation about Modulo(%) values
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int findSmallestInteger(vector<int>& nums, int value) {
+        unordered_map<int, int> mp;
+
+        for(int &num : nums) {
+            int r = ((num % value) + value) % value;
+
+            mp[r]++;
+        }
+
+        int MEX = 0;
+
+        while(mp[(MEX % value)] > 0) {
+            mp[(MEX % value)]--;
+
+            MEX++;
+        }
+
+        return MEX;
+    }
+};
