1c3238e2b7b317d42302194fd97718074d1cfec1
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-10-21 18:49:02 +0530

    Create Maximum Frequency of an Element After Performing Operations I.cpp

---
+++ Leetcode/Maximum Frequency of an Element After Performing Operations I.cpp
@@ -0,0 +1,43 @@
+//Leetcode Link : https://leetcode.com/problems/maximum-frequency-of-an-element-after-performing-operations-i
+
+//Approach - Using Cumulative Sum of Frequencies and trying all possible targets in nums
+//T.C : O(maxVal), maxVal = maximum element in nums + k
+//S.C : O(maxVal), maxVal = maximum element in nums + k
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        int maxEl = *max_element(begin(nums), end(nums)) + k;
+
+        vector<int> freq(maxEl + 1, 0);
+
+        for(int &num : nums) {
+            freq[num]++;
+        }
+
+        //cumulative sum of frequenceis
+        for(int i = 1; i <= maxEl; i++) {
+            freq[i] += freq[i-1];
+        }
+
+        int result = 0;
+        for(int target = 0; target <= maxEl; target++) {
+            if(freq[target] == 0)
+                continue;
+            
+            int leftNum  = max(0, target-k); //l
+            int rightNum = min(maxEl, target+k); //r
+
+            int totalCount  = freq[rightNum] - (leftNum > 0 ? freq[leftNum-1] : 0);
+
+            int targetCount = freq[target] - (target > 0 ? freq[target-1] : 0);
+
+            int needConversion = totalCount - targetCount;
+
+            int maxPossibleFreq = targetCount + min(needConversion, numOperations);
+
+            result = max(result, maxPossibleFreq);
+        }
+
+        return result;
+    }
+};
