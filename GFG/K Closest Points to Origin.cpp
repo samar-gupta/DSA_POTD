60d7c711bbe23df3835c2328ba6b38b7c744d79c
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-10-23 12:17:20 +0530

    Update K Closest Points to Origin.cpp

--- GFG/K Closest Points to Origin.cpp
+++ GFG/K Closest Points to Origin.cpp
@@ -1,3 +1,67 @@
+//Approach-1 : (using sorting with comarative function)
+class Solution {
+  public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        // code here
+        sort(points.begin(), points.end(), [&](vector<int>& A, vector<int>& B) {
+            return (A[0] * A[0] + A[1] * A[1]) < (B[0] * B[0] + B[1] * B[1]);
+        });
+        return vector<vector<int>>(points.begin(), points.begin() + k);
+    }
+};
+
+
+//Approach-2 : (Using maxHeap)
+class Solution {
+  public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        // code here
+        vector<vector<int>> ans;
+        priority_queue<pair<int, int>> pq;
+        int n = points.size();
+        for(int i=0; i<n; i++){
+            int x=points[i][0], y=points[i][1];
+            int dist=x*x+y*y;
+            pq.push({dist, i});
+            if(pq.size()>k) 
+                pq.pop();
+        }
+        
+        while(!pq.empty()){
+            ans.push_back(points[pq.top().second]);
+            pq.pop();
+        }
+        
+        return ans;
+    }
+};
+
+
+//Approach-3 : (Using minHeap)
+class Solution {
+  public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        // code here
+        vector<vector<int>> ans;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        int n=points.size();
+        for(int i=0; i<n; i++){
+            int x=points[i][0], y=points[i][1];
+            int dist=x*x+y*y;
+            pq.push({dist, i});
+        }
+        while(k > 0 && !pq.empty()){
+            ans.push_back(points[pq.top().second]);
+            pq.pop();
+            k--;
+        }
+        return ans;
+    }
+};
+
+
+
+//old
 class Solution {
   public:
   
