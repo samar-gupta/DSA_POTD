30e6c325df80e4f6d2812e25edb35c1319539349
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-11-01 16:45:52 +0530

    Update Delete Nodes From Linked List Present in Array.cpp

--- Leetcode/Delete Nodes From Linked List Present in Array.cpp
+++ Leetcode/Delete Nodes From Linked List Present in Array.cpp
@@ -1,7 +1,7 @@
 //Company Tags  : AMAZON
 //Leetcode Link : https://leetcode.com/problems/delete-nodes-from-linked-list-present-in-array/
 
-//Approach (Using Simple straight forward approach)
+//Approach (Using Simple straight forward approach) (asked again in potd, getting runtime error)
 //T.C : O(n)
 //S.C : O(n)
 class Solution {
@@ -23,6 +23,36 @@ public:
                 ListNode* temp = curr->next;
                 curr->next = curr->next->next; //curr->next is updated
                 delete(temp); //deleting hanging node
+            } else {
+                curr = curr->next;
+            }
+        }
+        
+        return head;
+    }
+};
+
+
+//Accepted and Working version
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        unordered_set<int> st(begin(nums), end (nums)); //inserted
+        
+        
+        //Handle the Head
+        while(head != NULL && st.find(head->val) != st.end()) {
+            //ListNode* temp = head;
+            head = head->next;
+            //delete(temp); //deleting the hanging node
+        }
+        
+        ListNode* curr = head;
+        while(curr != NULL && curr->next != NULL) {
+            if(st.find(curr->next->val) != st.end()) {
+                //ListNode* temp = curr->next;
+                curr->next = curr->next->next; //curr->next is updated
+                //delete(temp); //deleting hanging node
             } else {
                 curr = curr->next;
             }
