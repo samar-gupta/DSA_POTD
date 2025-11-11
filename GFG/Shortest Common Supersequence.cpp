498f8eb9aba533ed52043ef252a664fcc31aa94d
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-11-11 08:28:19 +0530

    Create Shortest Common Supersequence.cpp

---
+++ GFG/Shortest Common Supersequence.cpp
@@ -0,0 +1,21 @@
+//Approach-1 :
+class Solution {
+  public:
+    int minSuperSeq(string &s1, string &s2) {
+        // code here
+        int n=s1.length();
+        int m=s2.length();
+        
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1] == s2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return n+m-dp[n][m];
+    }
+};
