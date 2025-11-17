34f8679648aad7e95d427c2271e3a84bcdc5dfcb
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-11-17 09:33:58 +0530

    Create Check If All 1's Are at Least Length K Places Away.cpp

---
+++ Leetcode/Check If All 1's Are at Least Length K Places Away.cpp
@@ -0,0 +1,24 @@
+//Leetcode Link : https://leetcode.com/problems/check-if-all-1s-are-at-least-length-k-places-away
+
+//Approach - Maintain two pointers to count 0s between
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        int lastOne = -(k+1);
+
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == 1) {
+                if(i - lastOne - 1 < k)
+                    return false;
+                
+                lastOne = i;
+            }
+        }
+
+        return true;
+    }
+};
