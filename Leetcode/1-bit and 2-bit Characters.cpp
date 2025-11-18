142297dd190187d1cc39d3065b9ead77e5566daf
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-11-18 13:06:31 +0530

    Update 1-bit and 2-bit Characters.cpp

--- Leetcode/1-bit and 2-bit Characters.cpp
+++ Leetcode/1-bit and 2-bit Characters.cpp
@@ -1,4 +1,41 @@
-//Leetcode Link : 
+//Leetcode Link : https://leetcode.com/problems/1-bit-and-2-bit-characters/description/
+
+//Approach-1 (Iterating from left to right)
+//T.C : O(n) 
+//S.C : O(1)
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        int n = bits.size();
+        int i = 0;
+
+        while(i < n-1) {
+            i += (bits[i] == 1) ? 2 : 1;
+        }
+
+        return i == n-1;
+    }
+};
+
+
+//Approach-2 (Iterating from right to left and counting streak of 1s just after last 0)
+//T.C : O(n) 
+//S.C : O(1)
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        int n = bits.size();
+       
+        int count1 = 0;
+
+        for(int i = n-2; i >= 0 && bits[i] == 1; i--) {
+            count1++;
+        }
+
+        return count1%2 == 0 ? true : false;
+    }
+};
+
 
 //Approach-1 : 
 //T.C : O(n)
