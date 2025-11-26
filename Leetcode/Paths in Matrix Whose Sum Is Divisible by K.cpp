35fc73c7143bb0f12ab6e5a5e8e104a8c60bd9d8
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-11-26 12:05:51 +0530

    Create Paths in Matrix Whose Sum Is Divisible by K.cpp

---
+++ Leetcode/Paths in Matrix Whose Sum Is Divisible by K.cpp
@@ -0,0 +1,83 @@
+//Leetcode Link : https://leetcode.com/problems/paths-in-matrix-whose-sum-is-divisible-by-k
+
+//Approach-1 (Recursion + Memoization : DP On Grids)
+//T.C : O(m*n*k)
+//S.C : O(m*n*k)
+class Solution {
+public:
+    int M = 1e9 + 7;
+    vector<vector<vector<int>>> t;
+
+    int solve(int row, int col, int remain, vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        if(row >= m || col >= n)
+            return 0;
+
+        if(row == m - 1 && col == n - 1)
+            return ((remain + grid[row][col]) % k == 0);
+
+        if(t[row][col][remain] != -1)
+            return t[row][col][remain];
+
+        int newRemain = (remain + grid[row][col]) % k;
+
+        int down  = solve(row + 1, col, newRemain, grid, k);
+        int right = solve(row, col + 1, newRemain, grid, k);
+
+        return t[row][col][remain] = (down + right) % M;
+    }
+
+    int numberOfPaths(vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        t.assign(m, vector<vector<int>>(n, vector<int>(k, -1)));
+
+        return solve(0, 0, 0, grid, k);
+    }
+};
+
+
+//Approach-2 (Bottom Up  : DP On Grids)
+//T.C : O(m*n*k)
+//S.C : O(m*n*k)
+class Solution {
+public:
+    int M = 1e9 + 7;
+
+    int numberOfPaths(vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        int t[m+1][n+1][k+1];
+        memset(t, 0, sizeof(t));
+        // t[i][j][remain] = number of paths from [i][j] to [m-1][n-1] with current remainder = remain
+
+        // Base case same as recursion
+        for(int remain = 0; remain < k; remain++) {
+            t[m-1][n-1][remain] = ((remain + grid[m-1][n-1]) % k == 0 ? 1 : 0);
+        }
+
+        for(int row = m - 1; row >= 0; row--) {
+            for(int col = n - 1; col >= 0; col--) {
+
+                for(int remain = 0; remain < k; remain++) {
+
+                    if(row == m - 1 && col == n - 1)
+                        continue;
+
+                    int newRemain = (remain + grid[row][col]) % k;
+
+                    long long down  = t[row + 1][col][newRemain];
+                    long long right = t[row][col + 1][newRemain];
+
+                    t[row][col][remain] = (down + right) % M;
+                }
+            }
+        }
+
+        return t[0][0][0];
+    }
+};
