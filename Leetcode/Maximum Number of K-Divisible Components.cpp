70c9ead77718e79deb784558b28c147f27eac13a
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-11-28 17:19:33 +0530

    Update Maximum Number of K-Divisible Components.cpp

--- Leetcode/Maximum Number of K-Divisible Components.cpp
+++ Leetcode/Maximum Number of K-Divisible Components.cpp
@@ -1,5 +1,54 @@
 //Leetcode Link : https://leetcode.com/problems/maximum-number-of-k-divisible-components/
 
+//Approach (Using DFS) - You can easily replace DFS with BFS as well
+//T.C : O(n) - We visit all nodes once
+//S.C : O(n-1) ~ O(n) - We store edges in the adjacency list
+class Solution {
+public:
+    int count = 0;
+    int dfs(int curr, int parent,
+            unordered_map<int, vector<int>> &adj,
+            vector<int> &values, int k) {
+        
+
+        int sum = 0;
+
+        for (int &ngbr : adj[curr]) {
+            if (ngbr != parent) {
+                sum += dfs(ngbr, curr, adj, values, k);
+                sum %= k;
+            }
+        }
+
+        sum += values[curr];
+        sum %= k;
+
+        if (sum == 0) 
+            count++;
+
+        return sum;
+    }
+
+    int maxKDivisibleComponents(int n, vector<vector<int>> &edges,
+                                vector<int> &values, int k) {
+
+        unordered_map<int, vector<int>> adj;
+        for (auto &edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        count = 0;
+
+        dfs(0, -1, adj, values, k);
+
+        return count;
+    }
+};
+
+
 //Approach-1 :
 class Solution {
 public:
