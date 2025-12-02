25eb99f6dc6e80352ff6a67c343d6648548b61b6
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-12-02 17:33:46 +0530

    Create Count Number of Trapezoids I.cpp

---
+++ Leetcode/Count Number of Trapezoids I.cpp
@@ -0,0 +1,36 @@
+//Leetcode Link : https://leetcode.com/problems/count-number-of-trapezoids-i
+
+//Approach - Simple Maths counting
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int M = 1e9+7;
+
+    int countTrapezoids(vector<vector<int>>& points) {
+        unordered_map<int, int> mp; //y -> count
+
+        for(auto &point : points) {
+            int y = point[1];
+            mp[y]++;
+        }
+
+        long long result = 0; //countof trapezoids
+
+        long long prevHorizontalLines = 0;
+
+        for(auto &it : mp) {
+            int count = it.second;
+
+            long long horizontalLines = (long long)count * (count-1)/2; //countC2
+
+            result += horizontalLines * prevHorizontalLines;
+
+            prevHorizontalLines += horizontalLines;
+        }
+
+        return result % M;
+
+
+    }
+};
