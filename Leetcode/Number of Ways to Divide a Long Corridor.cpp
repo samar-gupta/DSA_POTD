f72ac10693c5cb9036acaf3df79d358c0a0500a0
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-12-14 16:28:00 +0530

    Create Number of Ways to Divide a Long Corridor.cpp

---
+++ Leetcode/Number of Ways to Divide a Long Corridor.cpp
@@ -0,0 +1,37 @@
+//Leetcode Link : https://leetcode.com/problems/number-of-ways-to-divide-a-long-corridor/
+
+//Approach-1 (Using simple iteration)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int mod = 1e9 + 7;
+    
+    int numberOfWays(string corridor) {
+        vector<int> pos_seats;
+        
+        for (int i = 0; i < corridor.size(); i++) {
+            if (corridor[i] == 'S') {
+                pos_seats.push_back(i);
+            }
+        }
+        
+        if (pos_seats.size() % 2 || pos_seats.size() == 0)
+            return 0;
+        
+        long long result = 1;
+        int prev         = pos_seats[1]; //End index of the starting subarray having 2 seats
+        
+        for (int i = 2; i < pos_seats.size(); i += 2) {
+            int length = pos_seats[i] - prev;
+            result     = (result * length) % mod;
+            
+            prev = pos_seats[i + 1];
+        }
+        
+        return result;
+    }
+};
+
+
+//Approach-2 (DP)
