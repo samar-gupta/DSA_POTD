483d0b55bf04f513adfe2f059ce700e1bbe8032f
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-12-16 14:13:34 +0530

    Update Strings Rotations of Each Other.cpp

--- GFG/Strings Rotations of Each Other.cpp
+++ GFG/Strings Rotations of Each Other.cpp
@@ -1,3 +1,51 @@
+//Approach:
+class Solution {
+  public:
+    bool areRotations(string &s1, string &s2) {
+        // code here
+        if ((s1+s1).find(s2)!=string::npos) 
+            return true;
+  
+        return false;
+    }
+};
+
+
+//Approach:
+class Solution {
+  public:
+    bool areRotations(string &s1, string &s2) {
+        // code here
+        if (s1.length() != s2.length())
+        return false;
+
+        string t = s1 + s1;
+        return t.find(s2) < t.length();
+    }
+};
+
+
+//Approach:
+class Solution {
+  public:
+    bool areRotations(string &s1, string &s2) {
+        // code here
+        s1 = s1 + s1;
+        int i = 0;
+        int j = 0;
+        while(i < s1.size() && j < s2.size()){
+            if(s1[i] == s2[j]){
+                j++;
+            }
+            i++;
+        }
+        if(j == s2.size()) return true;
+        return false;
+    }
+};
+
+
+//old
 class Solution {
   public:
     // Function to check if two strings are rotations of each other or not.
