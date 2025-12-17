8e9665a67151a879de1dc9224410a7fc6377dbf8
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2025-12-17 17:45:21 +0530

    Update Overlapping Intervals.cpp

--- GFG/Overlapping Intervals.cpp
+++ GFG/Overlapping Intervals.cpp
@@ -1,3 +1,23 @@
+//Approach-1:
+class Solution {
+  public:
+    vector<vector<int>> mergeOverlap(vector<vector<int>>& arr) {
+        // Code here
+        sort(arr.begin(),arr.end());
+        vector<vector<int>> ans;
+        ans.push_back(arr[0]);
+        for(int i=1;i<arr.size();i++)
+        {
+            if(ans.back()[1]>=arr[i][0])ans.back()[1]=max(arr[i][1],ans.back()[1]);
+            else ans.push_back(arr[i]);
+        }
+        
+        return ans;
+    }
+};
+
+
+//Approach-1:
 class Solution {
   public:
     vector<vector<int>> mergeOverlap(vector<vector<int>>& arr) {
