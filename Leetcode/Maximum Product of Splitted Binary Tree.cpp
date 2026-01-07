1289879ecdbbe1ab97e1f151f869ec0e62ddd553
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-01-07 16:40:25 +0530

    Create Maximum Product of Splitted Binary Tree.cpp

---
+++ Leetcode/Maximum Product of Splitted Binary Tree.cpp
@@ -0,0 +1,38 @@
+//Company Tags  : Google, Facebok, Amazon(Phone Interview)
+//Leetcode Link : https://leetcode.com/problems/maximum-product-of-splitted-binary-tree/
+
+//T.C : O(n)
+//S.C : O(n) if we consider recursion stack space, else O(1)
+class Solution {
+public:
+    
+    long M = 1e9+7;
+    long totalSum = 0, maxP = 0;
+    
+    int findTotalSum(TreeNode* root) {
+        if(!root)
+            return 0;
+        
+        int leftSubtreeSum  = findTotalSum(root->left);
+        int rightSubtreeSum = findTotalSum(root->right);
+        int sum             = root->val + leftSubtreeSum + rightSubtreeSum;
+        
+        maxP = max(maxP, (totalSum - sum) * sum);
+        
+        return sum;
+    }
+    
+    int maxProduct(TreeNode* root) {
+        if(!root)
+            return 0;
+        
+        maxP = 0;
+        
+        totalSum = findTotalSum(root);
+        
+        
+        findTotalSum(root);
+        
+        return maxP%M;
+    }
+};
