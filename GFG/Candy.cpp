55e5ffdb86a47b6fd374cdcd723a6647b12e340e
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-01-15 19:16:26 +0530

    Create Candy.cpp

---
+++ GFG/Candy.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int minCandy(vector<int> &ratings) {
+        // code here
+        int N = ratings.size();
+        vector<int> candies(N, 1);
+        for(int i = 1;i < N; i++) {
+            if(ratings[i] > ratings[i-1])
+                candies[i] = candies[i-1]+1;
+        }
+
+        for(int i = N-2;i >= 0; i--) {
+            if(ratings[i] > ratings[i+1])
+                candies[i] = max(candies[i], candies[i+1]+1);
+        }
+
+        int totalCandies = 0;
+        for(int candy : candies)
+            totalCandies += candy;
+
+        return totalCandies;
+    }
+};
