f5ae31c76e6eeeec804f4251c7b4286c0b8d3a83
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-01-19 16:28:33 +0530

    Update Remove K Digits.cpp

--- GFG/Remove K Digits.cpp
+++ GFG/Remove K Digits.cpp
@@ -1,3 +1,50 @@
+//Approach:
+class Solution {
+  public:
+    string removeKdig(string &s, int k) {
+        // code here
+        stack<char>st;
+        for(char ch:s)
+        {
+            while(!st.empty() && (st.top()>ch) && (k>0))
+            {
+                st.pop();
+                k--;
+            }
+            st.push(ch);
+        }
+        while(k>0 && !st.empty())
+        {
+            st.pop();
+            k--;
+        }
+        
+        string ans="";
+        while(!st.empty())
+        {
+            ans+=st.top();
+            st.pop();
+        }
+        int idx=-1;
+        reverse(ans.begin(),ans.end());
+        for(int i=0;i<ans.size();i++)
+        {
+            if(ans[i]!='0')
+            {
+                idx=i;
+                break;
+            }
+        }
+        if(idx==-1)
+        {
+            return "0";
+        }
+        return ans.substr(idx);
+    }
+};
+
+
+//old
 class Solution {
   public:
     string removeKdigits(string S, int k) {
