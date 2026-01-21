e2feb35b804089c20f96a6cee189723f3b0c9a2c
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-01-21 14:32:45 +0530

    Add Stock Span problem solution
    Implemented the Stock Span problem solution using a stack to calculate spans for stock prices.

--- GFG/Stock span problem.cpp
+++ GFG/Stock span problem.cpp
@@ -1,3 +1,32 @@
+//Approach:
+class Solution {
+  public:
+    vector<int> calculateSpan(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        vector<int>ans;
+        stack<int>st;
+        for(int i=0;i<n;i++)
+        {
+            while(!st.empty() && arr[st.top()]<=arr[i])
+            {
+                st.pop();
+            }
+            if(st.empty())
+            {
+                ans.push_back(i+1);
+            }
+            else{
+                ans.push_back(i-st.top());
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
+
+
+//old
 class Solution {
   public:
     vector<int> calculateSpan(vector<int>& price) {
