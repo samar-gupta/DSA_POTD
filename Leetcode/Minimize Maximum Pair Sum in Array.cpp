8fc9303c7c464997e07e945658ddf2ef2c981fd4
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-01-24 15:02:48 +0530

    Create Minimize Maximum Pair Sum in Array.cpp

---
+++ Leetcode/Minimize Maximum Pair Sum in Array.cpp
@@ -0,0 +1,28 @@
+//Company Tags                : AMAZON
+//Leetcode Link               : https://leetcode.com/problems/minimize-maximum-pair-sum-in-array/description/
+//Similar Leetcode Porblem    : Leetcode 881. Boats to Save People
+
+//Simple 2-pointer approach
+//T.C : O(nlogn) due to sorting
+//S.C : O(1)
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        sort(begin(nums), end(nums));
+
+        int maxResult = 0;
+        
+        int i = 0, j = nums.size()-1;
+        
+        while(i < j) {
+            int sum = nums[i] + nums[j];
+            
+            maxResult = max(maxResult, sum);
+            i++;
+            j--;
+        }
+        
+        return maxResult;
+        
+    }
+};
