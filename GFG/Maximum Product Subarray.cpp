ce570eb95e83887d4dc902723a1c9536253989c1
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-02-08 13:53:57 +0530

    Update Maximum Product Subarray.cpp

--- GFG/Maximum Product Subarray.cpp
+++ GFG/Maximum Product Subarray.cpp
@@ -1,3 +1,28 @@
+//Approach-1 : 
+class Solution {
+  public:
+    int maxProduct(vector<int> &arr) {
+        // code here
+        int n =arr.size();
+        int left=1,right=1;
+        int maxLeft=INT_MIN,maxRight=INT_MIN;
+        
+        for(int i=0;i<n;i++){
+            left*=arr[i];
+            maxLeft=max(maxLeft,left);
+            if(left==0) left=1;
+        }
+        for(int i=n-1;i>=0;i--){
+            right*=arr[i];
+            maxRight=max(maxRight,right);
+            if(right==0) right=1;
+        }
+        return max(maxLeft,maxRight);
+    }
+};
+
+
+//Approach-2 : 
 class Solution {
   public:
     // Function to find maximum product subarray
