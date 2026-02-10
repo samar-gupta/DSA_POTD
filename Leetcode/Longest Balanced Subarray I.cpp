1a527dff55eb637eba2f2a5d898cedad1c266d2d
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-02-10 08:33:57 +0530

    Create Longest Balanced Subarray I.cpp

---
+++ Leetcode/Longest Balanced Subarray I.cpp
@@ -0,0 +1,30 @@
+//Leetcode Link : https://leetcode.com/problems/longest-balanced-subarray-i
+
+//Approach (try all subarrays)
+//T.C : O(n^2)
+//S.C : O(n)
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int n = nums.size();
+
+        int maxLen = 0;
+
+        for(int i = 0; i < n; i++) {
+            unordered_set<int> even;
+            unordered_set<int> odd;
+
+            for(int j = i; j < n; j++) {
+                if(nums[j]%2 == 0)
+                    even.insert(nums[j]);
+                else
+                    odd.insert(nums[j]);
+                
+                if(even.size() == odd.size())
+                    maxLen = max(maxLen, j-i+1);
+            }
+        }
+
+        return maxLen;
+    }
+};
