74a125dd57b69c78c98d8ee59367c374331849a2
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-02-20 08:19:59 +0530

    Update Form the Largest Number.cpp

--- GFG/Form the Largest Number.cpp
+++ GFG/Form the Largest Number.cpp
@@ -26,3 +26,27 @@ class Solution {
         return result;
     }
 };
+
+
+
+//Approach-1 :
+class Solution {
+  public:
+    static bool compare(int a, int b){
+        string s1 = to_string(a);
+        string s2 = to_string(b);
+        
+        return s1 + s2 > s2 + s1;
+    }
+    
+    string findLargest(vector<int> &arr) {
+        // code here
+        sort(arr.begin(), arr.end(), compare);
+        string ans = "";
+        
+        for(auto &x : arr){
+            ans += to_string(x);
+        }
+        return ans[0] == '0' ? "0" : ans;
+    }
+};
