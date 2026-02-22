93db6d9efa8c1cdb0de7eda103c1af2f0a4a9bbf
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-02-22 11:50:39 +0530

    Add binary gap solutions in Binary Gap.cpp
    Implemented two approaches to find the binary gap of a number.

---
+++ Leetcode/Binary Gap.cpp
@@ -0,0 +1,50 @@
+//Company Tags  : Twitter, Amazon
+//Leetcode Link : https://leetcode.com/problems/binary-gap
+
+//Approach-1 (Simple bit checking)
+//T.C : ~O(32) i.e. O(1)
+//S.C : O(1)
+class Solution {
+public:
+    int binaryGap(int n) {
+        int curr = 0;
+        int prev = -1;
+
+        int result = 0;
+
+        while(n > 0) {
+            if((n&1) > 0) {
+                result = (prev != -1) ? max(result, curr - prev) : result;
+                prev = curr;
+            }
+
+            curr++;
+            n >>= 1;
+        }
+
+        return result;
+    }
+};
+
+
+
+//Approach-2 (Simple bit checking)
+//T.C : ~O(32) i.e. O(1)
+//S.C : O(1)
+class Solution {
+public:
+    int binaryGap(int n) {
+        int prev = -1;
+
+        int result = 0;
+
+        for(int curr = 0; curr < 32; curr++) {
+            if(((n >> curr) & 1) > 0) {
+                result = (prev != -1) ? max(result, curr-prev) : result;
+                prev = curr;
+            }
+        }
+
+        return result;
+    }
+};
