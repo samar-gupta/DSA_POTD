acb04a2a30967db7779dc4e5056c3301879059d4
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-02-23 08:27:31 +0530

    Update Union of Arrays with Duplicates.cpp

--- GFG/Union of Arrays with Duplicates.cpp
+++ GFG/Union of Arrays with Duplicates.cpp
@@ -1,5 +1,17 @@
 class Solution {
   public:
+    vector<int> findUnion(vector<int>& a, vector<int>& b) {
+        // code here
+        unordered_set<int>st(a.begin(),a.end());
+        st.insert(b.begin(),b.end());
+        return vector<int>(st.begin(),st.end());
+    }
+};
+
+
+// old
+class Solution {
+  public:
     // Function to return the count of number of elements in union of two arrays.
     int findUnion(vector<int>& a, vector<int>& b) {
         // code here
