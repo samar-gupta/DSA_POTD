b9c3e2db9f848d6bc86a82fe3b8efe7ee767f463
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-03-01 11:27:51 +0530

    Update Move All Zeroes to End.cpp

--- GFG/Move All Zeroes to End.cpp
+++ GFG/Move All Zeroes to End.cpp
@@ -1,18 +1,65 @@
+//Approach-1 :
 class Solution {
   public:
     void pushZerosToEnd(vector<int>& arr) {
         // code here
         int n = arr.size();
         int i=0;
-	      int j=0;
-	      while(j<n)
-        {
-	          if(arr[j]!=0)
-	          {
-	              swap(arr[i], arr[j]);
-	              i++;
-	          }
-	          j++;
-	      }
+	    int j=0;
+	    while(j<n)
+	    {
+	        if(arr[j]!=0)
+	        {
+	            swap(arr[i], arr[j]);
+	            i++;
+	        }
+	        j++;
+	    }
+    }
+};
+
+
+//Approach-2 : 
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        // code here
+        int slow=0;
+        for(int fast=0;fast<arr.size();fast++){
+            if(arr[fast]!=0){
+                arr[slow]=arr[fast];
+                slow++;
+            }
+            
+        }
+        
+        while(slow<arr.size()){
+            arr[slow]=0;
+            slow++;
+        }
+    }
+};
+
+
+// Approach-2 : 
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        // code here
+        int slow = 0;
+
+        // Move non-zero elements forward
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] != 0) {
+                arr[slow] = arr[i];
+                slow++;
+            }
+        }
+
+        // Fill remaining positions with zeros
+        for (int i = slow; i < arr.size(); i++) {
+            arr[i] = 0;
+        }
+
     }
 };
