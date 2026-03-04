f802fac9c96679f1ba1bf9de657daa2092dec0da
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-03-04 11:02:57 +0530

    Add maxSubarrayXOR function for maximum XOR calculation
    Implemented maxSubarrayXOR function to calculate maximum XOR of subarrays of size k.

--- GFG/Max Xor Subarray of size K.cpp
+++ GFG/Max Xor Subarray of size K.cpp
@@ -1,3 +1,23 @@
+//Approach-1 : 
+class Solution {
+  public:
+    int maxSubarrayXOR(vector<int>& arr, int k) {
+        // code here
+        int n = arr.size();
+        if (k > n) return 0;
+        int xorSum = 0;
+        for (int i = 0; i < k; ++i) xorSum ^= arr[i];
+        int maxXor = xorSum;
+        for (int i = k; i < n; ++i) {
+            xorSum ^= arr[i] ^ arr[i - k];
+            maxXor = max(maxXor, xorSum);
+        }
+        return maxXor;
+    }
+};
+
+
+//Approach-1 :
 class Solution {
   public:
     int maxSubarrayXOR(vector<int>& arr, int k) {
