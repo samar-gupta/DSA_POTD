50dc903f27654e104743f1dedb64d84f74badb05
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-03-29 12:32:47 +0530

    Create Check if Strings Can be Made Equal With Operations I.cpp

---
+++ Leetcode/Check if Strings Can be Made Equal With Operations I.cpp
@@ -0,0 +1,50 @@
+//Leetcode Link : https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-i
+
+//Approach-1 (Constant time check of 0,2 and 1,3 indices)
+//T.C : O(1)
+//S.C : O(1)
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        //index 0 and 2
+        bool condition1 = (s1[0] == s2[0] && s1[2] == s2[2]) || (s1[0] == s2[2] && s1[2] == s2[0]);
+
+
+        //index 1 and 3
+        bool condition2 = (s1[1] == s2[1] && s1[3] == s2[3]) || (s1[1] == s2[3] && s1[3] == s2[1]);
+
+        return condition1 && condition2;
+    }
+};
+
+
+
+//Approach-2 - Using map for grouping even and odd indices
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        int even[26] = {0};
+        int odd[26] = {0};
+
+        int n = s1.length();
+
+        for(int i = 0; i < 4; i++) {
+            if(i%2 == 0) { //even indices
+                even[s1[i] - 'a']++;
+                even[s2[i] - 'a']--;
+            } else { //odd indices
+                odd[s1[i] - 'a']++;
+                odd[s2[i] - 'a']--;
+            }
+        }
+
+        for(int i = 0; i < 26; i++) {
+            if(even[i] != 0 || odd[i] != 0)
+                return false;
+        }
+
+        return true;
+    }
+};
