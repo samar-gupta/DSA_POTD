fe1be6859fbcd9a07b921420ec1bf6ded3e0b813
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-03-29 12:16:27 +0530

    Refactor countPartitions and add new approach
    Refactor countPartitions function and add a new approach for partitioning with a given difference.

--- GFG/Partitions with Given Difference.cpp
+++ GFG/Partitions with Given Difference.cpp
@@ -1,8 +1,8 @@
+//Approach-1 : 
 class Solution {
   public:
-  
-  
     int mod=1e9+7;
+
     int solve(int i, int target, vector<int>&arr, vector<vector<int>>&dp){
         if(i==0){
             if(target==0){
@@ -22,8 +22,9 @@ class Solution {
         return dp[i][target];
     }
   
-    int countPartitions(int n, int d, vector<int>& arr) {
+    int countPartitions(vector<int>& arr, int d) {
         // Code here
+        int n = arr.size();
         int total=0;
         for(int i=0;i<n;i++){
             total+=arr[i];
@@ -36,5 +37,43 @@ class Solution {
         vector<vector<int>>dp(n+1, vector<int>(target+1, -1));
         
         return solve(n, target, arr, dp);
+    }
+};
+
+
+
+//Approach-2 :
+class Solution {
+  public:
+    int countPartitions(vector<int>& arr, int diff) {
+        // Code here
+        int totalSum = 0;
+        
+        // Calculate total sum of array
+        for (int num : arr) {
+            totalSum += num;
+        }
+        
+        // If totalSum + diff is odd, partition is impossible
+        if ((totalSum + diff) % 2 != 0) {
+            return 0;
+        }
+        
+        int target = (totalSum + diff) / 2;
+        
+        // dp[j] = number of ways to make sum j
+        vector<int> dp(target + 1, 0);
+        
+        // One way to make sum 0 -> choose nothing
+        dp[0] = 1;
+        
+        for (int num : arr) {
+            // Traverse backwards to avoid reusing same element
+            for (int j = target; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
+        
+        return dp[target];
     }
 };
