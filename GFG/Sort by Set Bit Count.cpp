ecba53a4fb32cd82610936478034315ad3f34316
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-05-03 09:35:43 +0530

    Refactor sortBySetBitCount function implementation

--- GFG/Sort by Set Bit Count.cpp
+++ GFG/Sort by Set Bit Count.cpp
@@ -1,12 +1,11 @@
 class Solution {
-  public:
-    static bool comp(int a, int b){
-        //ye ek inbuilt function hai jo set bit nikal kar deta hai c++ me
+public:
+    static bool comp(int a, int b) {
         return __builtin_popcount(a) > __builtin_popcount(b);
     }
-    
-    vector<int> sortBySetBitCount(vector<int>& arr) {
-        // code here
-        stable_sort(arr,arr+n,comp);
+
+    vector<int> sortBySetBitCount(vector<int> arr) {
+        stable_sort(arr.begin(), arr.end(), comp);
+        return arr;
     }
 };
