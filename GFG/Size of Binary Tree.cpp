a26c90f36f5fb0fb0bcf314902385b56765abe68
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-05-06 08:22:12 +0530

    Create Size of Binary Tree.cpp

---
+++ GFG/Size of Binary Tree.cpp
@@ -0,0 +1,21 @@
+/*
+Definition for Node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    int getSize(Node* root) {
+        // code here
+        if(!root) return 0;
+      return 1+getSize(root->left)+getSize(root->right);
+    }
+};
