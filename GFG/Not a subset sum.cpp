803e18aed3600d4882ed6bf48579f9c7854fc083
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-05-16 13:54:50 +0530

    Update Not a subset sum.cpp

--- GFG/Not a subset sum.cpp
+++ GFG/Not a subset sum.cpp
@@ -1,5 +1,29 @@
 class Solution {
   public:
+    int findSmallest(vector<int> &arr) {
+        // code here.
+        sort(arr.begin(), arr.end());
+
+        long long smallestMissing = 1;
+
+        for (int num : arr) {
+
+            if (num > smallestMissing) {
+                break;
+            }
+
+            smallestMissing += num;
+        }
+
+        return smallestMissing;
+    }
+};
+
+
+
+//old
+class Solution {
+  public:
     long long findSmallest(vector<int> &arr) {
         // Your code goes here.
         int ans=1;
