40ab627b30058afe50f1f26de6903a31f1fb823d
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-05-27 20:24:27 +0530

    Create Count the Number of Special Characters II.cpp

---
+++ Leetcode/Count the Number of Special Characters II.cpp
@@ -0,0 +1,41 @@
+//Company Tags           : will update later
+//Leetcode Link          : https://leetcode.com/problems/count-the-number-of-special-characters-ii/description/
+//Part I of this problem : http://youtube.com/post/UgkxA76EhTpRxCqK4apoG-YaaQ5Lao9VypAU?si=Gi0wKGIFA-kr9iaF
+
+//Approach (Track last occurrence of lowercase and first occurrence of uppercase)
+//T.C : O(n)
+//S.C : O(1) - fixed-size arrays of length 26
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        
+        vector<int> lastSmall(26, -1);
+        vector<int> firstCapital(26, -1);
+
+        for(int i = 0; i < word.length(); i++) {
+            
+            char ch = word[i];
+
+            if(islower(ch)) {
+                lastSmall[ch - 'a'] = i;
+            }
+            else {
+                if(firstCapital[ch - 'A'] == -1) {
+                    firstCapital[ch - 'A'] = i;
+                }
+            }
+        }
+
+        int count = 0;
+
+        for(int i = 0; i < 26; i++) {
+
+            if(lastSmall[i] != -1 && firstCapital[i] != -1 &&
+               lastSmall[i] < firstCapital[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
