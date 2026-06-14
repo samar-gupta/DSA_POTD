0806a454629caed6fd7abfa4fa7ad3de49c4f7f5
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-06-14 11:08:44 +0530

    Implement exit point logic in matrix solution

--- GFG/Exit Point in a Matrix.cpp
+++ GFG/Exit Point in a Matrix.cpp
@@ -1,3 +1,41 @@
+//Approach
+class Solution {
+  public:
+    vector<int> exitPoint(vector<vector<int>>& mat) {
+        // code here
+        int n = mat.size();
+        int m = mat[0].size();
+
+        int row = 0, col = 0;
+
+        int dir = 0;
+
+        while (row >= 0 && row < n && col >= 0 && col < m) {
+
+            if (mat[row][col] == 1) {
+
+                dir = (dir + 1) % 4;
+
+                mat[row][col] = 0;
+            }
+
+            if (dir == 0) col++;
+            else if (dir == 1) row++;
+            else if (dir == 2) col--;
+            else row--;
+        }
+
+        if (dir == 0) col--;
+        else if (dir == 1) row--;
+        else if (dir == 2) col++;
+        else row++;
+
+        return {row, col};
+    }
+};
+
+
+
 //Approach - 1
 class Solution {
   public:
