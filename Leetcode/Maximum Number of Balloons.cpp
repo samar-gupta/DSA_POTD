ff5202667dbff547ca6d6fc003774dbcc642d9e6
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-06-22 13:46:57 +0530

    Create Maximum Number of Balloons.cpp

---
+++ Leetcode/Maximum Number of Balloons.cpp
@@ -0,0 +1,23 @@
+//Leetcode Link : https://leetcode.com/problems/maximum-number-of-balloons/description/
+
+//Approach (Just find the one which is minimum)
+//T.C: O(n)
+//S.C: O(1)
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        int t[26] = {0};       
+        for(const char& ch: text) {
+            t[ch-'a']++;
+        }
+        
+        int count = INT_MAX;
+        count = min(count, t['b'-'a']);
+        count = min(count, t['a'-'a']);
+        count = min(count, t['l'-'a']/2);
+        count = min(count, t['o'-'a']/2);
+        count = min(count, t['n'-'a']);
+        
+        return count;
+    }
+};
