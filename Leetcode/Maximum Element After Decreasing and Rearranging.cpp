f08c360a84c32d990844ad7ca311b8a2571ec33c
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-06-28 12:01:59 +0530

    Add solution for maximum element problem
    Implement solution for maximum element after decreasing and rearranging.

---
+++ Leetcode/Maximum Element After Decreasing and Rearranging.cpp
@@ -0,0 +1,27 @@
+//Leetcode Link : https://leetcode.com/problems/maximum-element-after-decreasing-and-rearranging/
+
+//Apprpach-1 (Simply doing like asked in the Qn)
+//T.C : O(nlogn)
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort(begin(arr), end(arr));
+
+        int n = arr.size();
+        int maxEl = 1;
+
+        for(int i = 0; i < n; i++) {
+
+            if(i == 0) {
+                arr[i] = 1;
+            } else if(arr[i] - arr[i-1] > 1) {
+                arr[i] = arr[i-1] + 1;
+            }
+
+            maxEl = max(maxEl, arr[i]);
+        }
+
+        return maxEl;
+
+    }
+};
