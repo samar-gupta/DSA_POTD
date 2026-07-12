324592cec8f4d47d695cf9c3d83763debbc410a7
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-07-12 13:03:04 +0530

    Add rank transformation for an array
    Implemented array rank transformation using sorting and a map.

--- Leetcode/Rank Transform of an Array.cpp
+++ Leetcode/Rank Transform of an Array.cpp
@@ -21,3 +21,35 @@ public:
         return arr;
     }
 };
+
+
+
+//Approach - Sort and use map
+//T.C : O(nlogn)
+//S.C : O(n)
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        int n = arr.size();
+
+        vector<int> temp(arr);
+        sort(begin(temp), end(temp));
+
+        int rank = 1;
+        unordered_map<int, int> rankMap;
+        vector<int> result(n);
+
+        for(int i = 0; i < n; i++) {
+            if(i > 0 && temp[i] > temp[i-1]) {
+                rank++;
+            }
+            rankMap[temp[i]] = rank;
+        }
+
+        for(int i = 0; i < n; i++) {
+            result[i] = rankMap[arr[i]];
+        }
+
+        return result;
+    }
+};
