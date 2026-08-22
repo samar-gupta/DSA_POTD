05b02a37d6c2823f3faf363b2cabd03db3a07396
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-08-23 01:41:04 +0530

    Add Check Divisibility by Digit Sum and Product solution
    Implement a solution to check divisibility by digit sum and product.

---
+++ Leetcode/Check Divisibility by Digit Sum and Product.cpp
@@ -0,0 +1,21 @@
+//Leetcode Link : https://leetcode.com/problems/check-divisibility-by-digit-sum-and-product/description/
+
+//Approach :
+class Solution {
+public:
+    bool checkDivisibility(int n) {
+        int digitSum = 0;
+        int digitProduct = 1;
+        int original = n;
+
+        while (n > 0) {
+            int digit = n % 10;
+            n /= 10;
+
+            digitSum += digit;
+            digitProduct *= digit;
+        }
+
+        return original % (digitSum + digitProduct) == 0;
+    }
+};
