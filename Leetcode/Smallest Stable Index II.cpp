ef27e44ac4265ce275dd69acb502710ea607107e
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-09-04 13:03:22 +0530

    Create Smallest Stable Index II.cpp

---
+++ Leetcode/Smallest Stable Index II.cpp
@@ -0,0 +1,29 @@
+//Leetcode Link Part I  : https://leetcode.com/problems/smallest-stable-index-i/description/
+//Leetcode Link Part II : https://leetcode.com/problems/smallest-stable-index-ii/description/
+
+//Approach (just keep track of maxElement and store min element in pre-processing)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        vector<int> minFromIndex(n);
+        int minEl = INT_MAX;
+        for(int i = n-1; i >= 0; i--) {
+            minEl = min(minEl, nums[i]);
+            minFromIndex[i] = minEl;
+        }
+
+        int maxEl = 0;
+        for(int i = 0; i < n; i++) {
+            maxEl = max(maxEl, nums[i]);
+
+            if(maxEl - minFromIndex[i] <= k)
+                return i;
+        }
+        
+        return -1;
+    }
+};
