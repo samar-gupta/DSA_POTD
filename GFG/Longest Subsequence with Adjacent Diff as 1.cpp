2a5a892604bf1a71508ea514e1552ab669613906
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-09-05 12:53:44 +0530

    Update and rename Longest subsequence-1.cpp to Longest Subsequence with Adjacent Diff as 1.cpp

--- GFG/Longest Subsequence with Adjacent Diff as 1.cpp
+++ GFG/Longest Subsequence with Adjacent Diff as 1.cpp
@@ -19,3 +19,29 @@ class Solution {
         
     }
 };
+
+
+
+//Approach :
+class Solution {
+  public:
+    int longestSubseq(vector<int>& arr) {
+        // code here
+        unordered_map<int, int> dp;
+
+        int ans = 0;
+
+        for (int x : arr) {
+            int left = dp.count(x - 1) ? dp[x - 1] : 0;
+            int right = dp.count(x + 1) ? dp[x + 1] : 0;
+
+            int current = max(left, right) + 1;
+
+            dp[x] = current;
+
+            ans = max(ans, current);
+        }
+
+        return ans;
+    }
+};
