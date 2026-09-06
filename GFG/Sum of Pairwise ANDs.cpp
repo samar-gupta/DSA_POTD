2846c7902078ce9c48a1ea0062b27c91f5bb48dc
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-09-06 13:19:53 +0530

    Create Sum of Pairwise ANDs.cpp

---
+++ GFG/Sum of Pairwise ANDs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    long long pairAndSum(vector<int> &arr) {
+        // code here
+        ll ans=0;
+        int n=arr.size();
+        for(int j=0;j<32;j++)
+        {
+            ll count=0;
+            for(int i=0;i<n;i++)
+            {
+                ll curr=arr[i];
+                if((curr>>j)&1)
+                {
+                    count++;
+                }
+            }
+            ans+=(1LL<<j)*((count*1LL*(count-1))/2);
+        }
+        return ans;
+    }
+};
