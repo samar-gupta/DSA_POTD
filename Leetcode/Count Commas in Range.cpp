575ead471fbef04136d11ac78316a0e6e6beba42
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-09-08 08:53:22 +0530

    Create Count Commas in Range.cpp

---
+++ Leetcode/Count Commas in Range.cpp
@@ -0,0 +1,29 @@
+//Leetcode Link : https://leetcode.com/problems/count-commas-in-range
+
+//Approach-1 (Simple simulation)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    int countCommas(int n) {
+        int result = 0;
+        for (int num = 1; num <= n; num++) {
+            if (num > 999) {
+                result += 1;
+            }
+        }
+
+        return result;
+    }
+};
+
+
+//Approach-2 (Simple observation)
+//T.C : O(1)
+//S.C : O(1)
+class Solution {
+public:
+    int countCommas(int n) {
+        return max(n-999, 0);
+    }
+};
