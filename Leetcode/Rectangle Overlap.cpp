e87698dfb70d8a5642d23d86e3334431f97156cc
Samar Gupta <139665835+samar-gupta@users.noreply.github.com>
2026-09-14 09:15:26 +0530

    Update Rectangle Overlap.cpp

--- Leetcode/Rectangle Overlap.cpp
+++ Leetcode/Rectangle Overlap.cpp
@@ -8,7 +8,7 @@
 class Solution {
 public:
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
-        return recl[0] < rec2[2] && rec2[0] < recl[2] && 
-            recl[1] < rec2[3] && rec2[1] < recl[3];
+        return rec1[0] < rec2[2] && rec2[0] < rec1[2] && 
+            rec1[1] < rec2[3] && rec2[1] < rec1[3];
     }
 };
